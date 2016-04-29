03891acd8e02c0d1700cdcc4245d0758f42387dd
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-04-29 11:36:21 +0200

    added workflow using pyroSAR and newly created functions srtm.makeSRTm and gamma.util.geocode

---
+++ geoEngine.py
@@ -0,0 +1,33 @@
+
+from pyroSAR import identify
+from gamma.util import geocode
+
+srtmdir = "/geonfs02_vol1/SRTM_1_HGT"
+scenelist = "/geonfs01_vol1/ve39vem/scenelist"
+
+tempdir = "/geonfs01_vol1/ve39vem/temp"
+outdir = "/geonfs01_vol1/ve39vem/out"
+
+targetres = 20
+func_geoback = 2
+func_interp = 0
+scaling = "db"
+
+with open(scenelist, "r") as infile:
+    files = infile.read().strip().split()
+
+scenes = [identify(x) for x in files]
+
+# srtm_mosaic = os.path.join(tempdir, "srtm")
+#
+# srtm.makeSRTM(scenes, srtmdir, srtm_mosaic)
+#
+# srtm.transform(srtm_mosaic, srtm_mosaic+"_utm", targetres)
+#
+# srtm_mosaic += "_utm"
+
+srtm_mosaic = "/geonfs01_vol1/ve39vem/temp/srtm_swap_fill_utm"
+
+scene = scenes[0]
+
+geocode(scene, srtm_mosaic, tempdir, outdir, targetres, scaling, func_geoback, func_interp)
