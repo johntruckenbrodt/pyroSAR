19e3770d808901679d6ec999d7c6217479f1ae84
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-05-30 15:20:54 +0200

    integrated script geoEngine into swos_process

--- swos_process.py
+++ swos_process.py
@@ -1,16 +1,32 @@
 
-import re
+import os
 import spatial
 import vector
+import srtm
 from datetime import datetime
 from time import asctime, mktime, strptime
 from ancillary import finder, multicore
-
-from S1 import S1_geoGRD
+from gamma.util import geocode
 from pyroSAR import identify
 
-camarque = vector.Vector("/geonfs01_vol1/ve39vem/S1/test_camarque/boundary/Test_Sites_project_v11_Camarque.shp")
-camarque_area = camarque.getArea()
+sitename = "Camargue"
+maindir = "/geonfs01_vol1/ve39vem/swos_process"
+srtmdir = "/geonfs02_vol1/SRTM_1_HGT"
+
+targetres = 20
+func_geoback = 2
+func_interp = 0
+scaling = ["linear", "db"]
+
+tempdir = os.path.join(maindir, sitename, "proc_in")
+outdir = os.path.join(maindir, sitename, "proc_out")
+
+for dir in [tempdir, outdir]:
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+site = vector.Vector("/geonfs01_vol1/ve39vem/swos_testsites/Test_Sites_project_v29_{}.shp".format(sitename))
+site_area = site.getArea()
 
 scenes = finder("/geonfs02_vol1/swos_fsu/sentinel1/GRD", ["^S1A"], regex=True)
 # scenes = finder("/geonfs01_vol1/ve39vem/swos", ["*zip"])
@@ -20,7 +36,7 @@ a = datetime.now()
 for scene in scenes:
     try:
         id = identify(scene)
-        intersect = spatial.intersect(id.bbox(), camarque)
+        intersect = spatial.intersect(id.bbox(), site)
         if intersect is not None:
             if "VV" in id.polarisations and id.beam == "IW" and id.start > "20160302T054342":
             # if intersect.GetArea()/camarque_area == 1 and id.beam == "IW" and id.pols in ["SV", "DV"]:
@@ -55,14 +71,22 @@ print b-a
 #     print scenes[i].file, seconds(scenes[i])-seconds(scenes[i-1])
 
 
-print "started:", asctime()
-print "processing a total of {} scenes".format(len(selection))
-tempdir = "/geonfs01_vol1/ve39vem/S1/test_camarque/test_in"
-outdir = "/geonfs01_vol1/ve39vem/S1/test_camarque/test_out"
-srtmdir = "/geonfs02_vol1/SRTM_1_HGT"
+srtm_mosaic = os.path.join(maindir, sitename, "srtm_{}".format(sitename))
+srtm_mosaic_utm = srtm_mosaic + "_utm"
 
-multicore(S1_geoGRD.main, cores=10, multiargs={"zipfile": selection}, tempdir=tempdir, outdir=outdir, srtmdir=srtmdir, transform=True)
-print "finished:", asctime()
+if not os.path.isfile(srtm_mosaic):
+    srtm.makeSRTM(selection, srtmdir, srtm_mosaic)
 
-# testcase = "/geonfs02_vol1/swos_fsu/sentinel1/GRD/S1A_IW_GRDH_1SDV_20150314T173821_20150314T173846_005031_0064FC_DBE3.zip"
-# S1_geoGRD.main(testcase, tempdir=tempdir, outdir=outdir, srtmdir=srtmdir, transform=True)
+if not os.path.isfile(srtm_mosaic_utm):
+    srtm.transform(srtm_mosaic, srtm_mosaic_utm, targetres)
+
+# scene = selection[0]
+# geocode(scene, srtm_mosaic_utm, tempdir, outdir, targetres, scaling, func_geoback, func_interp)
+
+print "start geocoding"
+multicore(geocode, cores=10, multiargs={"scene": selection}, dem=srtm_mosaic_utm,
+          tempdir=tempdir, outdir=outdir,
+          targetres=targetres, scaling=scaling,
+          func_geoback=func_geoback, func_interp=func_interp)
+print datetime.now()
+print "done"
