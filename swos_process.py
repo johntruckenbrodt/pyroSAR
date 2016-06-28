e6de4a07246f94edbe8b907137e88e27783ef935
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-06-28 10:31:56 +0200

    general experimenting

--- swos_process.py
+++ swos_process.py
@@ -1,15 +1,15 @@
-
 import os
+import re
 import spatial
 import vector
 import srtm
 from datetime import datetime
-from time import asctime, mktime, strptime
+# from time import asctime, mktime, strptime
 from ancillary import finder, multicore
 from gamma.util import geocode
 from pyroSAR import identify
 
-sitename = "Camargue"
+sitename = "Azraq"
 maindir = "/geonfs01_vol1/ve39vem/swos_process"
 srtmdir = "/geonfs02_vol1/SRTM_1_HGT"
 
@@ -18,8 +18,9 @@ func_geoback = 2
 func_interp = 0
 scaling = ["linear", "db"]
 
-tempdir = os.path.join(maindir, sitename, "proc_in")
-outdir = os.path.join(maindir, sitename, "proc_out")
+sitedir = os.path.join(maindir, sitename)
+tempdir = os.path.join(sitedir, "proc_in")
+outdir = os.path.join(sitedir, "proc_out")
 
 for dir in [tempdir, outdir]:
     if not os.path.isdir(dir):
@@ -28,32 +29,34 @@ for dir in [tempdir, outdir]:
 site = vector.Vector("/geonfs01_vol1/ve39vem/swos_testsites/Test_Sites_project_v29_{}.shp".format(sitename))
 site_area = site.getArea()
 
-scenes = finder("/geonfs02_vol1/swos_fsu/sentinel1/GRD", ["^S1A"], regex=True)
+scenes = finder("/geonfs02_vol1/swos_fsu/sentinel1/GRD", ["^S1[AB]"], regex=True)
 # scenes = finder("/geonfs01_vol1/ve39vem/swos", ["*zip"])
+
+# selection = []
+# a = datetime.now()
+# for scene in scenes:
+#     try:
+#         id = identify(scene)
+#         if len(finder(outdir, [id.outname_base()], regex=True)) == 0:
+#             intersect = spatial.intersect(id.bbox(), site)
+#             if intersect is not None:
+#                 if "VV" in id.polarizations and id.acquisition_mode == "IW":
+#                     # if id.start > "20160302T054342":
+#                     # if intersect.GetArea()/site_area == 1:
+#                     # if id.beam == "IW" and id.pols in ["SV", "DV"]:
+#                     print scene
+#                     selection.append(id)
+#     except IOError:
+#         continue
 #
-selection = []
-a = datetime.now()
-for scene in scenes:
-    try:
-        id = identify(scene)
-        intersect = spatial.intersect(id.bbox(), site)
-        if intersect is not None:
-            if "VV" in id.polarisations and id.beam == "IW" and id.start > "20160302T054342":
-            # if intersect.GetArea()/camarque_area == 1 and id.beam == "IW" and id.pols in ["SV", "DV"]:
-            # if intersect.GetArea()/camarque_area == 1:
-            # if id.beam == "IW" and id.pols in ["SV", "DV"]:
-                print scene
-                selection.append(scene)
-    except IOError:
-        continue
-
-b = datetime.now()
-print b-a
-# with open("/geonfs01_vol1/ve39vem/scenelist", "w") as outfile:
-#     outfile.write("\n".join(selection)+"\n")
-
-# with open("/geonfs01_vol1/ve39vem/scenelist", "r") as infile:
-#     selection = infile.read().strip().split()
+# b = datetime.now()
+# print b - a
+#
+# with open(os.path.join(sitedir, "scenelist"), "w") as outfile:
+#     outfile.write("\n".join([x.scene for x in selection])+"\n")
+#
+with open(os.path.join(sitedir, "scenelist"), "r") as infile:
+    selection = [identify(x) for x in infile.read().strip().split()]
 
 # scenes = [identify(x) for x in selection]
 #
@@ -80,10 +83,11 @@ if not os.path.isfile(srtm_mosaic):
 if not os.path.isfile(srtm_mosaic_utm):
     srtm.transform(srtm_mosaic, srtm_mosaic_utm, targetres)
 
-# scene = selection[0]
+# scene = selection[1]
 # geocode(scene, srtm_mosaic_utm, tempdir, outdir, targetres, scaling, func_geoback, func_interp)
 
 print "start geocoding"
+print datetime.now()
 multicore(geocode, cores=10, multiargs={"scene": selection}, dem=srtm_mosaic_utm,
           tempdir=tempdir, outdir=outdir,
           targetres=targetres, scaling=scaling,
--- swos_resample.py
+++ swos_resample.py
@@ -1,29 +1,35 @@
 
+import os
 import re
 from raster import stack
 from ancillary import finder
 from time import mktime, strptime
 
-srcfiles = finder("/geonfs01_vol1/ve39vem/out", ["*_VV_*db.tif"])
-dstfile = "/geonfs01_vol1/ve39vem/Camarque_VV_dB_update"
-shp = "/geonfs01_vol1/ve39vem/S1/test_camarque/boundary/Test_Sites_project_v11_Camarque.shp"
+site = "Kilombero"
+maindir = "/geonfs01_vol1/ve39vem/swos_process"
 
+# store results in separate files
+sep = False
 
-def seconds(name): return mktime(strptime(re.findall("[0-9T]{15}", name)[0], "%Y%m%dT%H%M%S"))
+srcfiles = finder(os.path.join(maindir, site, "proc_out"), ["*_VV_*db.tif"])
 
-srcfiles = sorted(srcfiles, key=seconds)
+for i in range(6, 10):
+    dstfile = os.path.join(maindir, site, "{0}_VV_dB_{1}".format(site, i))
+    shp = "/geonfs01_vol1/ve39vem/swos_testsites/Test_Sites_project_v29_{0}_{1}.shp".format(site, i)
+    if os.path.isfile(dstfile):
+        raise IOError("dstfile already exists")
 
-groups = []
-temp = []
-
-for item in srcfiles:
-    if len(temp) == 0:
-        temp.append(item)
-    else:
-        if abs(seconds(item)-seconds(temp[-1])) < 30:
+    def seconds(name): return mktime(strptime(re.findall("[0-9T]{15}", name)[0], "%Y%m%dT%H%M%S"))
+    srcfiles = sorted(srcfiles, key=seconds)
+    groups = []
+    temp = []
+    for item in srcfiles:
+        if len(temp) == 0:
             temp.append(item)
         else:
-            groups.append(temp) if len(temp) > 1 else groups.append(temp[0])
-            temp = [item]
-
-stack(groups, dstfile, "bilinear", [20, 20], srcnodata=-99, dstnodata=-99, shapefile=shp, sortfun=seconds)
+            if 0 < abs(seconds(item)-seconds(temp[-1])) < 30:
+                temp.append(item)
+            else:
+                groups.append(temp) if len(temp) > 1 else groups.append(temp[0])
+                temp = [item]
+    stack(groups, dstfile, "bilinear", [20, 20], srcnodata=-99, dstnodata=-99, shapefile=shp, sortfun=seconds, separate=sep)
