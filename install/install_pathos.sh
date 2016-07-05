ce0c894f7735908dbdd78ffd27c0f83af7b86d60
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-07-05 18:33:27 +0200

    initial commit

---
+++ install/install_pathos.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#define (and create) a main directory in which all packages will be downloaded and installed
+maindir=$HOME/pathos_test
+
+#define the python interpreter/version to be used
+py=/usr/local/bin/python2.7
+
+#the download repository
+repository=dev.danse.us/packages
+
+#create the directory into which the python packages are going to be installed and add it to the PYTHONPATH variable
+mkdir -p $maindir/local/lib/python2.7/site-packages
+export PYTHONPATH=$maindir/local/lib/python2.7/site-packages:$PYTHONPATH
+
+#download the pathos package
+wget $repository/pathos-0.2a.dev-20130811.zip -P $maindir
+
+#unpack the pathos package
+unzip $maindir/pathos* -d $maindir
+
+#download some extra dependencies
+wget $repository/dill-0.2.4.zip -P $maindir/pathos*/external
+wget $repository/pox-0.2.2.zip -P $maindir/pathos*/external
+
+#unpack all the dependencies
+for archive in $maindir/pathos*/external/*.zip; do 
+	unzip $archive -d $maindir/pathos*/external; 
+done
+
+#install the dependency packages
+for dir in $maindir/pathos*/external/*/; do
+	cd $dir;
+	$py setup.py build;
+	$py setup.py install --prefix=$maindir/local
+done
+
+#alternative installation using pip (was found to cause errors)
+#$py /usr/local/bin/pip2.7 install --install-option="--prefix=${maindir}/local" dill pox
+#$py /usr/local/bin/pip2.7 install --user --upgrade --install-option="--prefix${maindir}/local" dill pox
+
+#install pathos
+cd $maindir/pathos*
+$py setup.py build
+$py setup.py install --prefix=$maindir/local
+
+#clean up
+rm -rf $maindir/pathos*
+
+echo please add the following line to your .bashrc:
+echo "export PYTHONPATH=${maindir}/local/lib/python2.7/site-packages:$"PYTHONPATH
+echo done
