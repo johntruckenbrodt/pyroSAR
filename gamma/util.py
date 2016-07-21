a98897784669dd2558456bd06145b65c12f772b2
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-07-21 14:47:57 +0200

    adaptation to new GAMMA version: snr now called ccp, thres not SNR but cross-correlation threshold, default value of thres changed from 7.0 to 0.15, docstrings adjusted accordingly function correlate: minwin changed from 128 to 16 function geocode: added second option to use simulated SAR image in SAR geometry for cross-correlation (instead of pixel area) syntactically simplified the code

--- gamma/util.py
+++ gamma/util.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ##############################################################
 # universal core routines for processing SAR images is GAMMA
-# John Truckenbrodt 2014-2015
+# John Truckenbrodt 2014-2016
 ##############################################################
 
 """
@@ -24,10 +24,10 @@ from ancillary import run, Stack, haversine, union
 # -off: diffpar parameter file
 # OUTPUT FILES:
 # -offs: offset estimates (fcomplex)
-# -offsets: range and azimuth offsets and SNR data (text format)
-# -snr: offset estimation SNR (float)
+# -offsets: range and azimuth offsets and cross-correlation data (text format)
+# -ccp: cross-correlation of each patch (0.0->1.0) (float)
 # -coffs: culled range and azimuth offset estimates (fcomplex)
-# -coffsets: culled offset estimates and SNR values (text format)
+# -coffsets: culled offset estimates and cross-correlation values (text format)
 # FURTHER INPUT:
 # -path_log: directory for created logfiles (e.g. offset_pwr will create file path_log/offset_pwr.log)
 # -maxwin: maximum (initial) window size for offset search (will be iteratively divided by 2 until the minwin is reached)
@@ -35,9 +35,9 @@ from ancillary import run, Stack, haversine, union
 # -overlap: percentage overlap of the search windows (the number of search windows is computed based on their windows size and the overlap)
 # -poly: the polynomial order for range and azimuth offset fitting
 # -ovs: image oversampling factor for offset estimation
-# -thres: offset estimation SNR quality threshold
+# -thres: cross-correlation threshold
 # the default value "-" will result in no output file written
-def correlate(master, slave, off, offs, snr, offsets="-", coffs="-", coffsets="-", path_log=None, maxwin=2048, minwin=128, overlap=.3, poly=4, ovs=2, thres=7.0):
+def correlate(master, slave, off, offs, ccp, offsets="-", coffs="-", coffsets="-", path_log=None, maxwin=2048, minwin=16, overlap=.3, poly=4, ovs=2, thres=0.15):
     """
     iterated cross-correlation offset and polynomial estimation
     this function is suited for SLCs (i.e. coregistration) as well as MLIs (i.e. geocoding); the procedure is selected based on the data type of the input data (complex for SLCs but not MLIs)
@@ -46,24 +46,21 @@ def correlate(master, slave, off, offs, snr, offsets="-", coffs="-", coffsets="-
     note: the implemented procedure is likely to be overly accurate for many applications; the procedure was chosen based on experience in extremely flat terrain where geocoding is
     problematic due to a lack of image contrast along topographic features and has proven to succeed even in those situations
     Args:
-        master: master image
-        slave: image coregistered to the master
+        (input) master: master image
+        (input) slave: image coregistered to the master
         off: diffpar parameter file
-        offs: offset estimates (fcomplex)
-        snr: offset estimation SNR (float)
-        offsets: range and azimuth offsets and SNR data (text format)
-        coffs: culled range and azimuth offset estimates (fcomplex)
-        coffsets: culled offset estimates and SNR values (text format)
+        (output) offs: offset estimates (fcomplex)
+        (output) ccp: cross-correlation of each patch (0.0->1.0) (float)
+        (output) offsets: range and azimuth offsets and cross-correlation data (text format)
+        (output) coffs: culled range and azimuth offset estimates (fcomplex)
+        (output) coffsets: culled offset estimates and cross-correlation values (text format)
         path_log: directory for created logfiles (e.g. offset_pwr will create file path_log/offset_pwr.log)
         maxwin: maximum (initial) window size for offset search (will be iteratively divided by 2 until the minwin is reached)
         minwin: minimum (final) window size
         overlap: percentage overlap of the search windows (the number of search windows is computed based on their windows size and the overlap)
         poly: the polynomial order for range and azimuth offset fitting
         ovs: image oversampling factor for offset estimation
-        thres: offset estimation SNR quality threshold
-
-    Returns:
-
+        thres: cross-correlation threshold
     """
 
     path_out = os.path.dirname(off)
@@ -90,17 +87,19 @@ def correlate(master, slave, off, offs, snr, offsets="-", coffs="-", coffsets="-
     passed = False
     winsize = maxwin
     while winsize >= minwin:
+        # compute the number of windows needed in range and azimuth based on the number of image pixels in both directions
+        nr = int(round((float(par.range_samples)/winsize)*(1+overlap)))
+        naz = str(int(int(nr)*dim_ratio))
+
+        args = [off, offs, ccp, winsize, winsize, offsets, ovs, nr, naz, thres]
         try:
-            # compute the number of windows needed in range and azimuth based on the number of image pixels in both directions
-            nr = int(round((float(par.range_samples)/winsize)*(1+overlap)))
-            naz = str(int(int(nr)*dim_ratio))
             if mode == "SLC":
-                run([commands[0], master, slave, master+".par", slave+".par", off, offs, snr, winsize, winsize, offsets, ovs, nr, naz, thres], path_out, path_log)
+                run([commands[0], master, slave, master+".par", slave+".par"]+args, path_out, path_log)
             else:
-                run([commands[0], master, slave, off, offs, snr, winsize, winsize, offsets, ovs, nr, naz, thres], path_out, path_log)
+                run([commands[0], master, slave]+args, path_out, path_log)
             passed = True
             try:
-                run([commands[1], offs, snr, off, coffs, coffsets, thres, poly], path_out, path_log)
+                run([commands[1], offs, ccp, off, coffs, coffsets, thres, poly], path_out, path_log)
             except RuntimeError:
                 passed = False
         except ValueError:
@@ -109,7 +108,7 @@ def correlate(master, slave, off, offs, snr, offsets="-", coffs="-", coffsets="-
             winsize /= 2
 
     if not passed:
-        for file in [offs, offsets, snr, coffs, coffsets]:
+        for file in [offs, offsets, ccp, coffs, coffsets]:
             if os.path.isfile(file):
                 os.remove(file)
         raise RuntimeError("cross-correlation failed; consider verifying scene overlap or choice of polarization")
@@ -123,7 +122,8 @@ def geocode(scene, dem, tempdir, outdir, targetres, scaling="linear", func_geoba
     dem: dem subsetted to the extent of the SAR image
     lut: rough geocoding lookup table
     lut_fine: fine geocoding lookup table
-    sim_sar: simulated SAR backscatter image in DEM geometry
+    sim_map: simulated SAR backscatter image in DEM geometry
+    sim_sar: simulated SAR backscatter image in SAR coordinates
     u: zenith angle of surface normal vector n (angle between z and n)
     v: orientation angle of n (between x and projection of n in xy plane)
     inc: local incidence angle (between surface normal and look vector)
@@ -133,8 +133,8 @@ def geocode(scene, dem, tempdir, outdir, targetres, scaling="linear", func_geoba
     off.par: ISP offset/interferogram parameter file
     offs: offset estimates (fcomplex)
     coffs: culled range and azimuth offset estimates (fcomplex)
-    coffsets: culled offset estimates and SNR values (text format)
-    snr: offset estimation SNR (float)
+    coffsets: culled offset estimates and cross correlation values (text format)
+    ccp: cross-correlation of each patch (0.0->1.0) (float)
     """
 
     scaling = [scaling] if isinstance(scaling, str) else scaling if isinstance(scaling, list) else []
@@ -165,6 +165,7 @@ def geocode(scene, dem, tempdir, outdir, targetres, scaling="linear", func_geoba
     dem_seg = master+"_dem"
     lut = master+"_lut"
     lut_fine = master+"_lut_fine"
+    sim_map = master+"_sim_map"
     sim_sar = master+"_sim_sar"
     u = master+"_u"
     v = master+"_v"
@@ -178,7 +179,7 @@ def geocode(scene, dem, tempdir, outdir, targetres, scaling="linear", func_geoba
     offs = master+"_offs"
     coffs = master+"_coffs"
     coffsets = master+"_coffsets"
-    snr = master+"_snr"
+    ccp = master+"_ccp"
     ellipse_pixel_area = master+"_ellipse_pixel_area"
     ratio_sigma0 = master+"_ratio_sigma0"
 
@@ -190,27 +191,43 @@ def geocode(scene, dem, tempdir, outdir, targetres, scaling="linear", func_geoba
 
     master_par = ISPPar(master+".par")
 
-    if master_par.image_geometry == "GROUND_RANGE":
-        run(["gc_map_grd", master+".par", dem+".par", dem, dem_seg+".par", dem_seg, lut, ovs_lat, ovs_lon, sim_sar, u, v, inc, psi, pix, ls_map, 8, func_interp], logpath=path_log)
-    else:
-        run(["gc_map", master+".par", "-", dem+".par", dem, dem_seg+".par", dem_seg, lut, ovs_lat, ovs_lon, sim_sar, u, v, inc, psi, pix, ls_map, 8, func_interp], logpath=path_log)
+    gc_map_opt = [dem+".par", dem, dem_seg+".par", dem_seg, lut, ovs_lat, ovs_lon, sim_map, u, v, inc, psi, pix, ls_map, 8, func_interp]
 
-    for item in [dem_seg, sim_sar, u, v, psi, pix, inc]:
+    if master_par.image_geometry == "GROUND_RANGE":
+        run(["gc_map_grd", master+".par"]+gc_map_opt, logpath=path_log)
+    else:
+        run(["gc_map", master+".par", "-"]+gc_map_opt, logpath=path_log)
+
+    for item in [dem_seg, sim_map, u, v, psi, pix, inc]:
         hdr(dem_seg+".par", item+".hdr")
 
     run(["pixel_area", master+".par", dem_seg+".par", dem_seg, lut, ls_map, inc, pixel_area], logpath=path_log)
 
-    correlate(master, pixel_area, off_par, offs, snr, coffs=coffs, coffsets=coffsets, offsets=offs+".txt", path_log=path_log, maxwin=256)
-
+    ######################################################################
+    # cross correlation approach 1 #######################################
+    # ####################################################################
+    # dempar = ISPPar(dem_seg + ".par")
+    # samples_dem = dempar.width
+    # samples_mli = master_par.range_samples
+    # lines_mli = master_par.azimuth_lines
+    # run(["geocode", lut, sim_map, samples_dem, sim_sar, samples_mli, lines_mli, func_interp], logpath=path_log)
+    # init_offset(master, sim_sar, off_par, path_log)
+    # correlate(master, sim_sar, off_par, offs, ccp, coffs=coffs, coffsets=coffsets, offsets=offs+".txt", path_log=path_log, maxwin=256)
+    ######################################################################
+    # cross correlation approach 2 #######################################
+    ######################################################################
+    init_offset(master, pixel_area, off_par, path_log)
+    correlate(master, pixel_area, off_par, offs, ccp, coffs=coffs, coffsets=coffsets, offsets=offs+".txt", path_log=path_log, maxwin=256)
+    ######################################################################
+    sim_width = ISPPar(dem_seg+".par").width
     try:
-        sim_width = ISPPar(dem_seg+".par").width
         run(["gc_map_fine", lut, sim_width, off_par, lut_fine, 0], logpath=path_log)
     except sp.CalledProcessError:
         print "...failed"
         return
 
     ######################################################################
-    # approach 1 #########################################################
+    # normalization and backward geocoding approach 1 ####################
     ######################################################################
     for image in images:
         run(["geocode_back", image, master_par.range_samples, lut_fine, image+"_geo", sim_width, 0, func_geoback], logpath=path_log)
@@ -218,20 +235,8 @@ def geocode(scene, dem, tempdir, outdir, targetres, scaling="linear", func_geoba
         run(["lin_comb", 1, image+"_geo_pix", 0, math.cos(math.radians(master_par.incidence_angle)), image+"_geo_pix_flat", sim_width], logpath=path_log)
         run(["sigma2gamma", image+"_geo_pix_flat", inc, image+"_geo_norm", sim_width], logpath=path_log)
         hdr(dem_seg+".par", image+"_geo_norm.hdr")
-
-        for scale in scaling:
-            if scale == "db":
-                run(["linear_to_dB", image+"_geo_norm", image+"_geo_norm_db", sim_width, 0, -99], logpath=path_log)
-                hdr(dem_seg+".par", image+"_geo_norm_db.hdr")
-            suffix = {"linear": "", "db": "_db"}[scale]
-            infile = image+"_geo_norm{}".format(suffix)
-            outfile = os.path.join(outdir, os.path.basename(image)+"_geo_norm{}.tif".format(suffix))
-            try:
-                run(["data2geotiff", dem_seg+".par", infile, 2, outfile], logpath=path_log)
-            except ImportWarning:
-                pass
     ######################################################################
-    # approach 2 #########################################################
+    # normalization and backward geocoding approach 2 ####################
     ######################################################################
     # try:
     #     run(["pixel_area", master+".par", dem_seg+".par", dem_seg, lut_fine, ls_map, inc, pixel_area2], logpath=path_log)
@@ -247,25 +252,22 @@ def geocode(scene, dem, tempdir, outdir, targetres, scaling="linear", func_geoba
     #     run(["lin_comb", 1, image+"_geo", 0, math.cos(math.radians(master_par.incidence_angle)), image+"_geo_flat", sim_width], logpath=path_log)
     #     run(["sigma2gamma", image+"_geo_flat", inc, image+"_geo_norm", sim_width], logpath=path_log)
     #     hdr(dem_seg+".par", image+"_geo_norm.hdr")
-    #
-    #     for scale in scaling:
-    #         suffix = {"linear": "", "db": "_db"}[scale]
-    #
-    #         if scale == "db":
-    #             run(["linear_to_dB", image+"_geo_norm", image+"_geo_norm_db", sim_width, 0, -99], logpath=path_log)
-    #             hdr(dem_seg+".par", image+"_geo_norm_db.hdr")
-    #         infile = image+"_geo_norm{}".format(suffix)
-    #         outfile = os.path.join(outdir, os.path.basename(image)+"_geo_norm{}.tif".format(suffix))
-    #         try:
-    #             run(["data2geotiff", dem_seg+".par", infile, 2, outfile], logpath=path_log)
-    #         except ImportWarning:
-    #             pass
     ######################################################################
-    ######################################################################
-    ######################################################################
+    for image in images:
+        for scale in scaling:
+            if scale == "db":
+                run(["linear_to_dB", image+"_geo_norm", image+"_geo_norm_db", sim_width, 0, -99], logpath=path_log)
+                hdr(dem_seg+".par", image+"_geo_norm_db.hdr")
+            suffix = {"linear": "", "db": "_db"}[scale]
+            infile = image+"_geo_norm{}".format(suffix)
+            outfile = os.path.join(outdir, os.path.basename(image)+"_geo_norm{}.tif".format(suffix))
+            try:
+                run(["data2geotiff", dem_seg+".par", infile, 2, outfile], logpath=path_log)
+            except ImportWarning:
+                pass
 
 
-def init_offset(master, slave, off, path_log, thres=7.0):
+def init_offset(master, slave, off, path_log, thres=0.15):
     """
     wrapper for iterated initial offset estimation between two SLCs or MLIs
     from a starting search window size of 128x128, initial offsets are search for; if this fails, the window size is increased up to a maximum size of 1024x1024
@@ -273,15 +275,6 @@ def init_offset(master, slave, off, path_log, thres=7.0):
     in case too few/no offsets were found on any window size, the routine will throw an error
     additionally, for each window size, the offset search is performed on three different levels of multilooking (factor 3, 2, 1) as this is reported to refine the results;
     if the routine fails on any level of multilooking, the routine will proceed with the next window size
-    Args:
-        master:
-        slave:
-        off:
-        path_log:
-        thres:
-
-    Returns:
-
     """
     path_out = os.path.dirname(off)
 
@@ -291,9 +284,12 @@ def init_offset(master, slave, off, path_log, thres=7.0):
     par = ISPPar(master + ".par")
     mode = "SLC" if par.image_format in ["FCOMPLEX", "SCOMPLEX"] else "MLI"
 
-    # first SLC offset estimation using orbit data (most important in case of very large offsets)
+    # first offset estimation using orbit state-vectors and image parameters (most important in case of very large offsets)
+    # this is only to be performed while coregistering two scenes not while geocoding
+    # todo: query input file type in order to decide whether command is executed
+    cmd = "init_offset_orbit" if mode == "SLC" else "init_offset_orbitm"
     if mode == "SLC":
-        run(["init_offset_orbit", master+".par", slave+".par", off], path_out, path_log)
+        run([cmd, master+".par", slave+".par", off], path_out, path_log)
 
     mlk = Spacing(par)
     passed = False
@@ -304,10 +300,11 @@ def init_offset(master, slave, off, path_log, thres=7.0):
         win = patchstack.pop()
         for factor in [3, 2, 1]:
             try:
+                args = [int(mlk.rlks)*factor, int(mlk.azlks)*factor, "-", "-", "-", "-", thres, win, win]
                 if mode == "SLC":
-                    run(["init_offset", master, slave, master+".par", slave+".par", off, int(mlk.rlks)*factor, int(mlk.azlks)*factor, "-", "-", "-", "-", thres, win, win], path_out, path_log)
+                    run(["init_offset", master, slave, master+".par", slave+".par", off]+args, path_out, path_log)
                 else:
-                    run(["init_offsetm", master, slave, off, int(mlk.rlks)*factor, int(mlk.azlks)*factor, "-", "-", "-", "-", thres, win], path_out, path_log)
+                    run(["init_offsetm", master, slave, off]+args, path_out, path_log)
                 passed = True
             except ValueError:
                 passed = False
