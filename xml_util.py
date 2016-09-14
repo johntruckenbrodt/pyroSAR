2720cc3839e363e1db8c6a34e14f184ed96dcfb2
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-09-14 10:50:25 +0200

    function getNamespaces: added support for multiple lines with namespaces

--- xml_util.py
+++ xml_util.py
@@ -3,17 +3,20 @@
 # John Truckenbrodt 2016
 ##############################################################
 
+from ancillary import dictmerge
+
 
 def getNamespaces(xml):
     namespaces = {}
     infile = xml if "readline" in dir(xml) else open(xml, "r")
     # calling 'for line in infile:' will cause an error later on!
+    namespaces = {}
     while True:
         line = infile.readline()
         if not line:
             break
-        if "xmlns" in line:
-            namespaces = dict([tuple(x.replace("xmlns:", "").replace('"', '').split("=")) for x in line.split() if x.startswith("xmlns")])
+        if "xmlns:" in line:
+            namespaces = dictmerge(namespaces, ([tuple(x.replace("xmlns:", "").replace('"', '').split("=")) for x in line.split() if x.startswith("xmlns")]))
     # reset file pointer
     infile.seek(0)
 
