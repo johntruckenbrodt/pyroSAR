8d830cf30eb8f43102231de0ae548f4a0738324b
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-09-21 14:13:58 +0200

    included Envisat, ERS-1 and ERS-2 integrated conversion to dB fixed bug in converting ENVI to GTiff format

--- snap/util.py
+++ snap/util.py
@@ -1,14 +1,26 @@
 
 import os
+import re
 import shutil
 import subprocess as sp
+from ancillary import finder
 from auxil import suffix_lookup, parse_recipe, write_recipe
 from pyroSAR import identify
 
 
 def geocode(infile, outdir, polarizations='all'):
 
+    orbit_lookup = {'ENVISAT': 'DELFT Precise (ENVISAT, ERS1&2) (Auto Download)',
+                    'SENTINEL-1': 'Sentinel Precise (Auto Download)'}
+
     id = identify(infile)
+
+    if id.sensor in ['ASAR', 'ERS1', 'ERS2']:
+        formatName = 'ENVISAT'
+    elif id.sensor in ['S1A', 'S1B']:
+        formatName = 'SENTINEL-1'
+    else:
+        raise RuntimeError('sensor not supported (yet)')
 
     workflow = parse_recipe('geocode')
 
@@ -25,21 +37,35 @@ def geocode(infile, outdir, polarizations='all'):
 
     if format == 'ENVI':
         outname = os.path.join(outdir, id.outname_base() + '_' + suffix)
-    elif format == 'GeoTiff-BigTIFF':
+    else:
         outname = os.path.join(outdir, '{}_{}_{}'.format(id.outname_base(), polarizations[0], suffix))
 
+    ############################################
     read = workflow.find('.//node[@id="Read"]')
     read.find('.//parameters/file').text = infile
-
+    read.find('.//parameters/formatName').text = formatName
+    ############################################
+    orb = workflow.find('.//node[@id="Apply-Orbit-File"]')
+    orb.find('.//parameters/orbitType').text = orbit_lookup[formatName]
+    ############################################
     cal = workflow.find('.//node[@id="Calibration"]')
-    cal.find('.//parameters/selectedPolarisations').text = ','.join(polarizations)
-
+    if id.sensor in ['ERS1', 'ERS2'] or (id.sensor == 'ASAR' and id.acquisition_mode != 'APP'):
+        cal.find('.//parameters/selectedPolarisations').text = 'Intensity'
+    else:
+        cal.find('.//parameters/selectedPolarisations').text = ','.join(polarizations)
+    ############################################
     tf = workflow.find('.//node[@id="Terrain-Flattening"]')
-    tf.find('.//parameters/sourceBands').text = ','.join(['Beta0_'+x for x in polarizations])
-
+    if id.sensor in ['ERS1', 'ERS2'] or (id.sensor == 'ASAR' and id.acquisition_mode != 'APP'):
+        tf.find('.//parameters/sourceBands').text = 'Beta0'
+    else:
+        tf.find('.//parameters/sourceBands').text = ','.join(['Beta0_'+x for x in polarizations])
+    ############################################
     tc = workflow.find('.//node[@id="Terrain-Correction"]')
     tc.find('.//parameters/sourceBands').text = ','.join(['Gamma0_'+x for x in polarizations])
-
+    ############################################
+    dB = workflow.find('.//node[@id="LinearToFromdB"]')
+    dB.find('.//parameters/sourceBands').text = ','.join(['Gamma0_' + x for x in polarizations])
+    ############################################
     write = workflow.find('.//node[@id="Write"]')
     write.find('.//parameters/file').text = outname
     write.find('.//parameters/formatName').text = format
@@ -57,10 +83,10 @@ def geocode(infile, outdir, polarizations='all'):
         sp.check_call(['gpt', outname+'_proc.xml'])
 
     if format == 'ENVI':
-        for pol in polarizations:
-            name_old = os.path.join(outname, 'Gamma0_{}'.format(pol))
+        for item in finder(outname, ['*.img']):
+            pol = re.search('[HV]{2}', item).group()
             name_new = os.path.join(outdir, '{}_{}_{}'.format(id.outname_base(), pol, suffix))
             sp.check_call(['gdal_translate', '-q', '-of', 'GTiff',
                            '-co', 'INTERLEAVE=BAND', '-co', 'TILED=YES',
-                           name_old+'.img', name_new+'.tif'])
+                           item, name_new + '.tif'])
         shutil.rmtree(outname)
