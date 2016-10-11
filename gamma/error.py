73abe8c6e21fda63ea41864089076de7142f422f
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-10-11 14:05:18 +0200

    minor style edits

--- gamma/error.py
+++ gamma/error.py
@@ -3,22 +3,22 @@ import re
 
 
 def gammaErrorHandler(message):
-    errormessage = message.strip().replace("ERROR: ", "")
-    knownErrors = {"image data formats differ": IOError,
-                   "cannot open": IOError,
-                   "no coverage of SAR image by DEM \(in (?:latitude/northing|longitude/easting)\)": IOError,
-                   "libgdal.so.1: no version information available": None,
-                   "line outside of image": ValueError,
-                   "no offsets found above SNR threshold": ValueError,
-                   "window size < 4": ValueError,
-                   "MLI oversampling factor must be 1, 2, 4, 8": ValueError,
-                   "no points available for determining average intensity": ValueError,
-                   "insufficient offset points to determine offset model parameters": RuntimeError,
-                   "insufficient offset points left after culling to determine offset model parameters": RuntimeError,
-                   "calloc_1d: number of elements <= 0": ValueError,
-                   "multi-look output line:": RuntimeError,
-                   "gc_map operates only with slant range geometry, image geometry in SLC_par: GROUND_RANGE": RuntimeError}
-    if errormessage != "":
+    errormessage = message.strip().replace('ERROR: ', '')
+    knownErrors = {'image data formats differ': IOError,
+                   'cannot open': IOError,
+                   'no coverage of SAR image by DEM \(in (?:latitude/northing|longitude/easting)\)': IOError,
+                   'libgdal.so.1: no version information available': None,
+                   'line outside of image': ValueError,
+                   'no offsets found above SNR threshold': ValueError,
+                   'window size < 4': ValueError,
+                   'MLI oversampling factor must be 1, 2, 4, 8': ValueError,
+                   'no points available for determining average intensity': ValueError,
+                   'insufficient offset points to determine offset model parameters': RuntimeError,
+                   'insufficient offset points left after culling to determine offset model parameters': RuntimeError,
+                   'calloc_1d: number of elements <= 0': ValueError,
+                   'multi-look output line:': RuntimeError,
+                   'gc_map operates only with slant range geometry, image geometry in SLC_par: GROUND_RANGE': RuntimeError}
+    if errormessage != '':
         for error in knownErrors:
             if re.search(error, errormessage):
                 errortype = knownErrors[error]
