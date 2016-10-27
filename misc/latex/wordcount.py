f1fa05228ea78dc2aa01d5a03e17a81f77a7b9f2
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2016-10-27 12:11:33 +0200

    minor style changes

--- misc/latex/wordcount.py
+++ misc/latex/wordcount.py
@@ -10,23 +10,23 @@ def dissolve(inlist):
         out.extend(dissolve(i)) if type(i) is list else out.append(i)
     return out
 
-files = ["D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/MasterThesis_Main.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_conclusion.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_introduction.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_discussion.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_interferometry.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_AGB.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_setup.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_theory.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_THF.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_orbitmasking.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_polarimetry.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_sampling.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_sar.tex",
-         "D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_sarprocessing.tex"]
+files = ['D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/MasterThesis_Main.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_conclusion.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_introduction.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_discussion.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_interferometry.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_AGB.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_setup.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_theory.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_modeling_THF.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_orbitmasking.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_polarimetry.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_sampling.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_sar.tex',
+         'D:/Dokumente/Studium/Master/GEO512-Masterarbeit/Latex/modules/chapters/MasterThesis_sarprocessing.tex']
 
-sp.check_call(dissolve(["perl", "D:/TeXcount_3_0_0_24/texcount.pl", "-merge", files]))
+sp.check_call(dissolve(['perl', 'D:/TeXcount_3_0_0_24/texcount.pl', '-merge', files]))
 
 # end 1307: 12189/139/970  -> 13298
 # end 1407: 13424/139/1063 -> 14626 (+1328)
