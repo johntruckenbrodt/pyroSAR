ec85618b0c70c0215fa220ecb869a21d60a073cd
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2017-01-24 16:06:05 +0100

    introduced new argument outfile giving an alternative to just printing to the screen

--- S1/S1_burstmatch.py
+++ S1/S1_burstmatch.py
@@ -29,6 +29,7 @@
  ****************************************************************************
 """
 
+import os
 import re
 import argparse
 from osgeo import ogr
@@ -42,7 +43,7 @@ def slc_burst_corners(parfile, tops_parfile):
     proc = sp.Popen(['SLC_burst_corners', parfile, tops_parfile], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
     out, err = proc.communicate()
     if proc.returncode != 0:
-        raise RuntimeError(err+'\nreading of burst corners failed')
+        raise RuntimeError(err + '\nreading of burst corners failed')
     matches = [re.findall('[0-9.]+', x) for x in re.findall('Burst:[0-9 .]*', out)]
     bursts = {}
     for line in matches:
@@ -58,7 +59,7 @@ def coord2geom(coordinatelist):
     return ogr.CreateGeometryFromWkt('POLYGON(({}))'.format(', '.join(coordstrings)))
 
 
-def burstoverlap(slc1_parfile, slc2_parfile, slc1_tops_parfile, slc2_tops_parfile):
+def burstoverlap(slc1_parfile, slc1_tops_parfile, slc2_parfile, slc2_tops_parfile):
     b1 = slc_burst_corners(slc1_parfile, slc1_tops_parfile)
     b2 = slc_burst_corners(slc2_parfile, slc2_tops_parfile)
     master_select = []
@@ -75,7 +76,7 @@ def burstoverlap(slc1_parfile, slc2_parfile, slc1_tops_parfile, slc2_tops_parfil
     return master_select, slave_select
 
 
-def main(slc1_par, slc2_par, slc1_topspar, slc2_topspar):
+def main(slc1_par, slc2_par, slc1_topspar, slc2_topspar, outfile):
     print ''
     print 'SLC1_PAR:      ', slc1_par
     print 'SLC1_TOPS_PAR: ', slc1_topspar
@@ -83,21 +84,27 @@ def main(slc1_par, slc2_par, slc1_topspar, slc2_topspar):
     print 'SLC2_TOPS_PAR: ', slc2_topspar
     slc1_select, slc2_select = burstoverlap(slc1_par, slc2_par, slc1_topspar, slc2_topspar)
 
-    if len(slc1_select+slc2_select) == 0:
-        print 'no overlap'
+    if len(slc1_select + slc2_select) == 0:
+        outstring = 'no overlap'
     else:
-        print '\nIDs of overlapping bursts:'
-        print 'SLC1:', ', '.join(map(str, slc1_select))
-        print 'SLC2:', ', '.join(map(str, slc2_select))
-    print ''
+        outstring = '\nIDs of overlapping bursts:\n' \
+                   'SLC1:{0}\nSLC2:{1}\n'.format(', '.join(map(str, slc1_select)),
+                                                 ', '.join(map(str, slc2_select)))
+
+    if outfile:
+        with open(outfile, 'w') as out:
+            out.write(outstring)
+    else:
+        print outstring
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='This program returns the IDs of overlapping bursts of two S1 TOPS burst SLC swaths. '
                     'The returned IDs are to be used by the Gamma program SLC_copy_S1_TOPS to crop the two swaths to their common overlap.')
+    parser.add_argument("-of", "--outfile", default=None, help="a textfile which the result is written to", type=str)
     parser.add_argument('SLC1_PAR', help='(input) SLC-1 parameter file for the TOPS burst SLC')
     parser.add_argument('SLC1_TOPS_PAR', help='(input) SLC-1 TOPS parameter file for the TOPS burst SLC')
     parser.add_argument('SLC2_PAR', help='(input) SLC-2 parameter file for the TOPS burst SLC')
     parser.add_argument('SLC2_TOPS_PAR', help='(input) SLC-2 TOPS parameter file for the TOPS burst SLC')
     args = parser.parse_args()
-    main(args.SLC1_PAR, args.SLC2_PAR, args.SLC1_TOPS_PAR, args.SLC2_TOPS_PAR)
+    main(args.SLC1_PAR, args.SLC1_TOPS_PAR, args.SLC2_PAR, args.SLC2_TOPS_PAR, args.outfile)
