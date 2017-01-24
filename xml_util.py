f0c7fd51076ffb175df029f96bcb3e7aae72330b
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2017-01-24 15:51:46 +0100

    function getNamespaces: reset line counter before looping over the lines

--- xml_util.py
+++ xml_util.py
@@ -7,16 +7,20 @@ from ancillary import dictmerge
 
 
 def getNamespaces(xml):
-    namespaces = {}
-    infile = xml if "readline" in dir(xml) else open(xml, "r")
+    infile = xml if 'readline' in dir(xml) else open(xml, 'r')
     # calling 'for line in infile:' will cause an error later on!
     namespaces = {}
+    infile.seek(0)
     while True:
         line = infile.readline()
         if not line:
             break
-        if "xmlns:" in line:
-            namespaces = dictmerge(namespaces, ([tuple(x.replace("xmlns:", "").replace('"', '').split("=")) for x in line.split() if x.startswith("xmlns")]))
+        if 'xmlns:' in line:
+            namespaces = dictmerge(namespaces,
+                                   ([tuple(x.replace('xmlns:', '')
+                                           .replace('"', '')
+                                           .split('='))
+                                     for x in line.split() if x.startswith('xmlns')]))
     # reset file pointer
     infile.seek(0)
 
