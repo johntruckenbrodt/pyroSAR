d892df7d32ac381ec694cef4ee0ff86a0c55794b
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2017-02-23 15:54:46 +0100

    function csrConvert: added prettyWkt as output type

--- spatial/auxil.py
+++ spatial/auxil.py
@@ -1,3 +1,7 @@
+##############################################################
+# Convenience functions for general spatial applications
+# John Truckenbrodt, 2016-2017
+##############################################################
 import math
 import os
 from osgeo import osr
@@ -8,17 +12,17 @@ osr.UseExceptions()
 def crsConvert(crsIn, crsOut):
     """
     convert between epsg, wkt, proj4 and opengis spatial references
-    crsText must be a osr.SpatialReference object, an opengis URL (e.g. "http://www.opengis.net/def/crs/EPSG/0/4326") or a string of type WKT, PROJ4 or EPSG
-    crsOut must be either "wkt", "proj4", "epsg", "osr" or "opengis"
-    if type "osr" is selected the function will return a spatial reference object of type osr.SpatialReference()
+    crsText must be a osr.SpatialReference object, an opengis URL (e.g. 'http://www.opengis.net/def/crs/EPSG/0/4326') or a string of type WKT, PROJ4 or EPSG
+    crsOut must be either 'wkt', 'proj4', 'epsg', 'osr', 'opengis' or 'prettyWkt' (a wkt string formatted for readability)
+    if type 'osr' is selected the function will return a spatial reference object of type osr.SpatialReference()
     """
     if isinstance(crsIn, osr.SpatialReference):
         srs = crsIn.Clone()
     else:
         srs = osr.SpatialReference()
         try:
-            if "opengis.net/def/crs/EPSG/0/" in crsIn:
-                crsIn = int(os.path.basename(crsIn.strip("/")))
+            if 'opengis.net/def/crs/EPSG/0/' in crsIn:
+                crsIn = int(os.path.basename(crsIn.strip('/')))
             srs.ImportFromEPSG(crsIn)
         except (TypeError, RuntimeError):
             try:
@@ -27,21 +31,23 @@ def crsConvert(crsIn, crsOut):
                 try:
                     srs.ImportFromProj4(crsIn)
                 except (TypeError, RuntimeError):
-                    raise TypeError("crsText not recognized; must be of type WKT, PROJ4 or EPSG")
-    if crsOut == "wkt":
+                    raise TypeError('crsText not recognized; must be of type WKT, PROJ4 or EPSG')
+    if crsOut == 'wkt':
         return srs.ExportToWkt()
-    elif crsOut == "proj4":
+    elif crsOut == 'prettyWkt':
+        return srs.ExportToPrettyWkt()
+    elif crsOut == 'proj4':
         return srs.ExportToProj4()
-    elif crsOut == "epsg":
+    elif crsOut == 'epsg':
         srs.AutoIdentifyEPSG()
         return int(srs.GetAuthorityCode(None))
-    elif crsOut == "opengis":
+    elif crsOut == 'opengis':
         srs.AutoIdentifyEPSG()
-        return "http://www.opengis.net/def/crs/EPSG/0/{}".format(srs.GetAuthorityCode(None))
-    elif crsOut == "osr":
+        return 'http://www.opengis.net/def/crs/EPSG/0/{}'.format(srs.GetAuthorityCode(None))
+    elif crsOut == 'osr':
         return srs
     else:
-        raise ValueError("crsOut not recognized; must be either wkt, proj4 or epsg")
+        raise ValueError('crsOut not recognized; must be either wkt, proj4, opengis or epsg')
 
 
 def haversine(lat1, lon1, lat2, lon2):
