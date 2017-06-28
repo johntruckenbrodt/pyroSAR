2c9466f353ec1b00595bfbbf07ceec868bc87f56
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2017-06-28 15:03:52 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='pyroSAR',
+      packages=find_packages(),
+      version='0.1',
+      description='The funniest joke in the world',
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+      ],
+      url='https://github.com/johntruckenbrodt/pyroSAR.git',
+      author='John Truckenbrodt',
+      author_email='john.truckenbrodt@uni-jena.de',
+      license='MIT',
+      zip_safe=False)
