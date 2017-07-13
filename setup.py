dff0135bece04568758ef7edcc39ab2adef27163
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2017-07-13 15:55:33 +0200

    added option include_package_data=True, added requirement progressbar

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(name='pyroSAR',
       packages=find_packages(),
+      include_package_data=True,
       version='0.1',
       description='a framework for large-scale SAR satellite data processing',
       classifiers=[
           'Programming Language :: Python :: 2.7',
       ],
+      install_requires=['progressbar==2.3'],
       url='https://github.com/johntruckenbrodt/pyroSAR.git',
       author='John Truckenbrodt',
       author_email='john.truckenbrodt@uni-jena.de',
