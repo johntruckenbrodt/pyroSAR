55e5b48623aafb9c268030f521e69cc7124f9144
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2017-07-19 02:30:26 +0200

    added description

--- examples/example_stacking.py
+++ examples/example_stacking.py
@@ -5,7 +5,7 @@
 import os
 
 from pyroSAR.ancillary import finder, groupbyTime, seconds
-from pyroSAR.spatial import stack, Vector
+from pyroSAR.spatial import stack
 
 
 def main():
@@ -21,21 +21,23 @@ def main():
     # store results in separate files or one single stack file? If separate then dstfile is used as a directory.
     sep = True
 
-    # define
+    # list files to be resampled; those not overlapping with the shapefile geometry will excluded by function stack
     srcfiles = finder(dir_in, ['S1*_VV_*norm_db.tif'])
 
+    # check whether dstfile is already a file
     if os.path.isfile(dstfile):
         raise IOError('dstfile already exists')
 
-    site = Vector(shp)
-
-    # create groups of similar time stamps for mosaicking. All images with a time stamp of less than 30s difference will be mosaicked
+    # create groups of similar time stamps for mosaicking.
+    # All images with a time stamp of less than 30s difference will be grouped
     groups = groupbyTime(srcfiles, seconds, 30)
 
     # final function call
+    # groups will be mosaicked first
+    # the resulting images will all have the same extent
     stack(srcfiles=groups, dstfile=dstfile, resampling='bilinear',
           targetres=[20, 20], srcnodata=-99, dstnodata=-99,
-          shapefile=site, sortfun=seconds, separate=sep, overwrite=False)
+          shapefile=shp, sortfun=seconds, separate=sep, overwrite=False)
 
 if __name__ == '__main__':
     main()
