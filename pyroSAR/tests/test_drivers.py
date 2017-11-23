0e788bb954104c86118b818386cafc4f8a3fe3f6
Felix Cremer <felix.cremer@uni-jena.de>
2017-11-23 11:08:45 +0100

    Make path to file from pyroSAR

--- pyroSAR/tests/test_drivers.py
+++ pyroSAR/tests/test_drivers.py
@@ -4,7 +4,7 @@ import unittest
 import pytest
 
 testcases = [
-    {"path": "data/S1A_IW_GRDH_1SDV_20150222T170750_20150222T170815_004739_005DD8_3768.zip",
+    {"path": "pyroSAR/tests/data/S1A_IW_GRDH_1SDV_20150222T170750_20150222T170815_004739_005DD8_3768.zip",
      "compression": "zip",
      "sensor": "S1A",
      "product": "GRD",
