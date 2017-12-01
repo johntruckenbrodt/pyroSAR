4b9871007bc43751b11832c5335d704f22582474
Ismail Baris <i.baris@outlook.de>
2017-12-01 09:17:00 +0100

    Add executable check

--- pyroSAR/snap/__init__.py
+++ pyroSAR/snap/__init__.py
@@ -1,3 +1,11 @@
-
 from .util import geocode
-from .auxil import getAuxdata, gpt
+from .auxil import getAuxdata, gpt, check_executable
+import warnings
+
+SNAP_EXECUTABLE = ['snap32.exe', 'snap64.exe', 'snap.exe', 'snap']
+
+if check_executable(SNAP_EXECUTABLE):
+    pass
+else:
+    warnings.warn("Lorem ipsum dolor sit amet, consectetuer adipiscing elit: http://step.esa.int/main/download/", 
+                  Warning)
