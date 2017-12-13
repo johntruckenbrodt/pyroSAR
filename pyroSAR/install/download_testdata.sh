068ee3b81a2d34e5d20b4b14b484aeb9f37fe3b4
Felix Cremer <felix.cremer@uni-jena.de>
2017-12-13 14:52:31 +0100

    Go back into previous folder after download of testdata

--- pyroSAR/install/download_testdata.sh
+++ pyroSAR/install/download_testdata.sh
@@ -9,3 +9,4 @@ echo "Start Download forest_brazil"
 wget ftp://ftp.eorc.jaxa.jp/pub/ALOS-2/1501sample/310_forestbrazil/0000022708_001001_ALOS2015976960-140909.zip
 echo "End download forest_brazil"
 
+cd -
