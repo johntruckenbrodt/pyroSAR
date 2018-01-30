c429e5663cc1b3e6ea067a8cf8d207a9923f6967
felixcremer <felix.cremer@uni-jena.de>
2018-01-30 09:23:27 +0100

    Use wget -P to select download folder and not cd -

--- pyroSAR/install/download_testdata.sh
+++ pyroSAR/install/download_testdata.sh
@@ -3,10 +3,8 @@
 
 mkdir -p $TESTDATA_DIR
 
-cd $TESTDATA_DIR
+#cd $TESTDATA_DIR
 
 echo "Start Download forest_brazil"
-wget ftp://ftp.eorc.jaxa.jp/pub/ALOS-2/1501sample/310_forestbrazil/0000022708_001001_ALOS2015976960-140909.zip
+wget -P $TESTDATA_DIR ftp://ftp.eorc.jaxa.jp/pub/ALOS-2/1501sample/310_forestbrazil/0000022708_001001_ALOS2015976960-140909.zip 
 echo "End download forest_brazil"
-
-cd -
