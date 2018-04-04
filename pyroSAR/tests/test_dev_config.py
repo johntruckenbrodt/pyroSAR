9cea1d02b0db058434355d05786ba31e38a70155
Ismail Baris <i.baris@outlook.de>
2018-04-04 09:43:54 +0200

    Update unit test due to python 3 errors.

--- pyroSAR/tests/test_dev_config.py
+++ pyroSAR/tests/test_dev_config.py
@@ -12,6 +12,7 @@ class TestStorage:
     def test_keys(self):
         storage = Storage(a=1, b=2)
         key = storage.keys()
+        key = list(key)
         assert key[0] == 'a'
         assert key[1] == 'b'
 
