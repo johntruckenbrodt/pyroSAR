f064868c931473d1722e30708b46e42f893a6637
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-05-17 18:16:04 +0200

    remove xml workflow created for testing

--- pyroSAR/tests/test_snap.py
+++ pyroSAR/tests/test_snap.py
@@ -1,7 +1,9 @@
 
+import os
 from pyroSAR.snap import geocode
 
 
 def test_geocode():
     scene = 'pyroSAR/tests/data/S1A_IW_GRDH_1SDV_20150222T170750_20150222T170815_004739_005DD8_3768.zip'
     geocode(scene, 'pyroSAR/tests/data', test=True)
+    os.remove('pyroSAR/tests/data/S1A__IW___A_20150222T170750_bnr_Orb_Cal_TF_TC_dB_proc.xml')
