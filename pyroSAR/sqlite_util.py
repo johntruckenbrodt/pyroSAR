3cbddc85638497cc3415393ee79f098c82e05e76
John Truckenbrodt <john_truckenbrodt@uni-jena.de>
2018-06-03 13:32:18 +0200

    handler class improvements and fixes:
    - fixed error in looping through spatialite library options
    - introduced new attributes extensions and version
    - added parameter defaults to __init__

--- pyroSAR/sqlite_util.py
+++ pyroSAR/sqlite_util.py
@@ -30,11 +30,19 @@ def sqlite_setup(driver=':memory:', extensions=None):
 
 
 class __Handler(object):
-    def __init__(self, driver, extensions):
+    def __init__(self, driver=':memory:', extensions=None):
         self.conn = sqlite3.connect(driver)
         self.conn.enable_load_extension(True)
-        for ext in extensions:
-            self.load_extension(ext)
+        self.extensions = []
+        if isinstance(extensions, list):
+            for ext in extensions:
+                self.load_extension(ext)
+
+    @property
+    def version(self):
+        spatialite_cursor = self.conn.execute('''SELECT spatialite_version()''')
+        spatialite_version = spatialite_cursor.fetchall()[0][0].encode('ascii')
+        return {'sqlite': sqlite3.version, 'spatialite': spatialite_version}
 
     def get_tablenames(self):
         cursor = self.conn.execute('''SELECT * FROM sqlite_master WHERE type="table"''')
@@ -48,6 +56,9 @@ class __Handler(object):
                 try:
                     self.conn.load_extension(option)
                     select = option
+                    self.extensions.append(option)
+                    print('loading extension {0} as {1}'.format(extension, option))
+                    break
                 except sqlite3.OperationalError:
                     continue
             if select is None:
@@ -62,3 +73,4 @@ class __Handler(object):
                 raise RuntimeError('no library found for extension {}'.format(extension))
             print('loading extension {0} as {1}'.format(extension, ext_mod))
             self.conn.load_extension(ext_mod)
+            self.extensions.append(ext_mod)
