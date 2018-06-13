8596eec3398d451eb7eeadf3a78e14a5af6f4f37
Ismail Baris <i.baris@outlook.de>
2018-06-13 13:03:59 +0200

    [pyrosar._dev_config, snap.auxil] Add unit tests.

--- pyroSAR/tests/test_snap_exe.py
+++ pyroSAR/tests/test_snap_exe.py
@@ -1,9 +1,11 @@
+import os
 from contextlib import contextmanager
 
 import pytest
+from os.path import expanduser
 
-from pyroSAR._dev_config import ExamineExe
-from pyroSAR.snap.auxil import ExamineSnap
+from pyroSAR._dev_config import ExamineExe, make_dir, write_config_file
+from pyroSAR.snap.auxil import ExamineSnap, get_etc_from_config
 
 
 @contextmanager
@@ -46,3 +48,29 @@ class TestExamineSnap:
     #     with pytest.warns(None) as record:
     #         ExamineSnap()
     #     assert len(record) == 0
+
+
+class TestSnapExeAuxil:
+    def test_make_dir(self):
+        path = expanduser("~")
+        make_dir(path)
+        dir = os.listdir(path)
+        assert '.pyrosar' in dir
+
+    def test_write_config(self):
+        path = os.path.join(expanduser("~"), '.pyrosar')
+        write_config_file('test_data', header=None, path=path)
+
+        dir = os.listdir(path)
+
+        assert 'config.txt' in dir
+
+    def read_config(self):
+        path = expanduser("~")
+        make_dir(path)
+
+        write_config_file(data='test_path', header='snap_etc:', path=os.path.join(expanduser("~"), '.pyrosar'))
+
+        content = get_etc_from_config()
+
+        assert content == 'test_path'
