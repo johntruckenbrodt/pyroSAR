ca8e9ab902459975cb20affd4273e92eaddec9d0
Ismail Baris <i.baris@outlook.de>
2018-07-05 18:39:18 +0200

    [_dev_config: New Class Config] Add Configuration Management System.  - Add keys, values and variables to Storage instance.

--- pyroSAR/_dev_config.py
+++ pyroSAR/_dev_config.py
@@ -8,9 +8,16 @@ import os
 import warnings
 from distutils.spawn import find_executable
 from os.path import expanduser
+import ConfigParser
+import io
 
 __LOCAL__ = ['sensor', 'projection', 'orbit', 'polarizations', 'acquisition_mode',
              'start', 'stop', 'product', 'spacing', 'samples', 'lines']
+
+__CONFIG_NANE__ = "config.ini"
+__PATH__ = os.path.join(expanduser("~"), '.pyrosar')
+
+__CONFIG__ = os.path.join(__PATH__, __CONFIG_NANE__)
 
 
 class Storage(dict):
@@ -105,47 +112,42 @@ LOOKUP = Storage(snap=snap,
                              'vh': 'INTEGER'})
 
 # URL
-dem = Storage(ace2='http://step.esa.int/auxdata/dem/ACE2/5M/',
-              ace='http://step.esa.int/auxdata/dem/ACE30/',
-              srtm3_FTP='xftp.jrc.it',
-              srtm3='http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/',
-              srtm1Hgt='http://step.esa.int/auxdata/dem/SRTMGL1/', )
+dem_url = Storage(ace2='http://step.esa.int/auxdata/dem/ACE2/5M/',
+                  ace='http://step.esa.int/auxdata/dem/ACE30/',
+                  srtm3_FTP='xftp.jrc.it',
+                  srtm3='http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/',
+                  srtm1Hgt='http://step.esa.int/auxdata/dem/SRTMGL1/', )
 
-orbit = Storage(doris='http://step.esa.int/auxdata/orbits/Doris/vor',
-                ers1='http://step.esa.int/auxdata/orbits/ers_precise_orb/ERS1',
-                ers2='http://step.esa.int/auxdata/orbits/ers_precise_orb/ERS2',
-                s1_poe='http://step.esa.int/auxdata/orbits/Sentinel-1/POEORB/',
-                s1_res='http://step.esa.int/auxdata/orbits/Sentinel-1/RESORB/')
+orbit_url = Storage(doris='http://step.esa.int/auxdata/orbits/Doris/vor',
+                    ers1='http://step.esa.int/auxdata/orbits/ers_precise_orb/ERS1',
+                    ers2='http://step.esa.int/auxdata/orbits/ers_precise_orb/ERS2',
+                    s1_poe='http://step.esa.int/auxdata/orbits/Sentinel-1/POEORB/',
+                    s1_res='http://step.esa.int/auxdata/orbits/Sentinel-1/RESORB/')
 
-auxcal = Storage(s1='http://step.esa.int/auxdata/auxcal/S1/',
-                 envisat='http://step.esa.int/auxdata/auxcal/ENVISAT/',
-                 ers='http://step.esa.int/auxdata/auxcal/ERS/')
+auxcal_url = Storage(s1='http://step.esa.int/auxdata/auxcal/S1/',
+                     envisat='http://step.esa.int/auxdata/auxcal/ENVISAT/',
+                     ers='http://step.esa.int/auxdata/auxcal/ERS/')
 
-URL = Storage(dem=dem,
-              orbit=orbit,
-              auxcal=auxcal)
+URL = Storage(dem=dem_url,
+              orbit=orbit_url,
+              auxcal=auxcal_url)
+
+# Set Variable Names
+SECTION = Storage(snap='SNAP')
+
+KEY_SNAP = Storage(etc_path='etc_path',
+                   auxdata_path='auxdata_path',
+                   config_path='config_path')
+
+KEY = Storage(snap=KEY_SNAP)
+
+CONFIG = Storage(section=SECTION,
+                 key=KEY)
 
 # Merge
 STORAGE = Storage(URL=URL,
-                  LOOKUP=LOOKUP)
-
-
-# Auxiliary functions
-def make_dir(path=expanduser("~"), dirname='.pyrosar'):
-    directory = os.path.join(path, dirname)
-
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-
-def write_config_file(data, header, path=os.path.join(expanduser("~"), '.pyrosar')):
-    files = os.path.join(path, 'config.txt')
-
-    with open(files, 'a') as x_file:
-        if header is None:
-            x_file.write('{0} \n'.format(data))
-        else:
-            x_file.write('{0}:{1} \n'.format(header, data))
+                  LOOKUP=LOOKUP,
+                  CONFIG=CONFIG)
 
 
 # Class Definitions
@@ -183,3 +185,103 @@ class ExamineExe(object):
                 warnings.warn(
                     "One of the executables {0} should be installed. You can download it from http://step.esa.int/main/toolboxes/snap/ or you can specify a path with snap_config.set_path(path_to_snap)".format(
                         name), UserWarning)
+
+
+class Config(object):
+    def __init__(self, config_file=__CONFIG__, path=__PATH__):
+        self.cfg = config_file
+        self.cfgpath = path
+
+        Config.__make_dir(self.cfgpath)
+        self.__create_config()
+
+    @staticmethod
+    def __make_dir(path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        else:
+            pass
+
+    def __create_config(self):
+        if not os.path.isfile(self.cfg):
+            with open(self.cfg, 'w'):
+                pass
+        else:
+            pass
+
+    def print_config(self):
+        cfgp = ConfigParser.RawConfigParser()
+        cfgp.read(self.cfg)
+
+        print("List all contents:")
+
+        for section in cfgp.sections():
+            print("\n Section: {0}".format(section))
+
+            for options in cfgp.options(section):
+                print("\t x {0} :: {1} :: {2}".format(options, cfgp.get(section, options), str(type(options))))
+
+    def add_section(self, section):
+        cfgp = ConfigParser.RawConfigParser()
+        cfgp.read(self.cfg)
+
+        if section not in cfgp.sections():
+            cfgp.add_section(section)
+            with open(self.cfg, 'wb') as cfg:
+                cfgp.write(cfg)
+
+        else:
+            pass
+            # warnings.warn("Section already exists.")
+
+    def set(self, section, variable, value, overwrite=True):
+        cfgp = ConfigParser.RawConfigParser()
+        cfgp.read(self.cfg)
+
+        if section not in cfgp.sections():
+            raise AttributeError(
+                "The section name is not in the config file. You must define it with self.create_section().")
+        else:
+            if variable in cfgp.options(section):
+                if overwrite:
+                    cfgp.set(section, variable, value)
+                    with open(self.cfg, 'wb') as cfg:
+                        cfgp.write(cfg)
+                else:
+                    warnings.warn("Value already exists.")
+
+            else:
+                cfgp.set(section, variable, value)
+                with open(self.cfg, 'wb') as cfg:
+                    cfgp.write(cfg)
+
+    def get(self, section, key=None):
+        cfgp = ConfigParser.RawConfigParser()
+        cfgp.read(self.cfg)
+
+        if section not in cfgp.sections():
+            raise AttributeError(
+                "The section name is not in the config file. You must define it with self.create_section().")
+        else:
+            if key is None:
+                return dict(cfgp.items(section))
+
+            else:
+                items = dict(cfgp.items(section))
+                return items[key]
+
+
+# config = Config()
+#
+# config.print_config()
+# config.get('SNAP', CONFIG_VAR.snap.etc_path)
+#
+# config.add_section('SNAP')
+# config.set('SNAP', 'etc_path', __PATH__)
+# config.set('SNAP', 'auxdata_path', 'some/path/to/auxdata')
+# config.set('SNAP', 'snap_path', __PATH__, overwrite=True)
+# config.set('SNAP', 'etc_path', 'change', overwrite=False)
+#
+# config.add_section('ENVI')
+# config.set('ENVI', 'path', 'some/path/to/envi')
