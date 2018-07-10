c495f94c34e9a4d56c161fa005785d6783f7fb78
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-07-10 12:54:51 +0200

    [gamma.auxil.ISPPar] new methods __str__ and envidict

--- pyroSAR/gamma/auxil.py
+++ pyroSAR/gamma/auxil.py
@@ -1,10 +1,13 @@
-
+##############################################################
+# general GAMMA utilities
+# John Truckenbrodt 2014-2018
+##############################################################
 import math
 import os
 import re
 import subprocess as sp
 
-from ..ancillary import parse_literal, run
+from ..ancillary import parse_literal, run, union
 from .error import gammaErrorHandler
 
 
@@ -32,6 +35,9 @@ class ISPPar(object):
             par_file = open(filename, 'r')
         else:
             par_file = filename
+
+        self.keys = []
+
         try:
             par_file.readline()  # Skip header line
             for line in par_file:
@@ -61,19 +67,63 @@ class ISPPar(object):
                                 if len(value) == 1:
                                     value = value[0]
                                 break
+                self.keys.append(key)
                 setattr(self, key, value)
         finally:
             par_file.close()
+
+    def __str__(self):
+        maxlen = len(max(self.keys, key=len)) + 1
+        return '\n'.join(['{key}:{sep}{value}'.format(key=key,
+                                                      sep=(maxlen - len(key)) * ' ',
+                                                      value=getattr(self, key)) for key in self.keys])
+
+    def envidict(self):
+        out = dict(bands=1,
+                   header_offset=0,
+                   file_type='ENVI Standard',
+                   interleave='bsq',
+                   sensor_type='Unknown',
+                   byte_order=1,
+                   wavelength_units='Unknown')
+
+        out['samples'] = getattr(self, union(['width', 'range_samples', 'samples'], self.keys)[0])
+        out['lines'] = getattr(self, union(['nlines', 'azimuth_lines', 'lines'], self.keys)[0])
+
+        # 2x16 bit complex data (SCOMPLEX) is not supported by ENVI, thus float (1x32 bit) is written to the header file
+        dtypes = {'FCOMPLEX': 6, 'SCOMPLEX': 4, 'FLOAT': 4, 'REAL*4': 4, 'INTEGER*2': 2, 'SHORT': 12}
+        out['data_type'] = dtypes[getattr(self, union(['data_format', 'image_format'], self.keys)[0])]
+
+        if out['data_type'] == 6:
+            out['complex_function'] = 'Power'
+        # projections = ['AEAC', 'EQA', 'LCC', 'LCC2', 'OMCH', 'PC', 'PS', 'SCH', 'TM', 'UTM']
+        if hasattr(self, 'DEM_projection'):
+            if self.DEM_projection == 'UTM':
+                hem = 'North' if float(self.false_northing) == 0 else 'South'
+                out['map_info'] = ['UTM', '1.0000', '1.0000',
+                                   self.corner_east, self.corner_north,
+                                   str(abs(float(self.post_east))), str(abs(float(self.post_north))),
+                                   self.projection_zone, hem, 'WGS-84', 'units=Meters']
+            elif self.DEM_projection == 'EQA':
+                out['map_info'] = ['Geographic Lat/Lon', '1.0000', '1.0000',
+                                   self.corner_lon, self.corner_lat,
+                                   str(abs(float(self.post_lon))), str(abs(float(self.post_lat))),
+                                   'WGS-84', 'units=Degrees']
+            else:
+                raise RuntimeError('unsupported projection')
+        return out
 
 
 class UTM(object):
     """
     convert a gamma parameter file corner coordinate from EQA to UTM
     """
+
     def __init__(self, parfile):
         par = ISPPar(parfile)
         inlist = [str(x) for x in ["WGS84", 1, "EQA", par.corner_lon, par.corner_lat, "", "WGS84", 1, "UTM", ""]]
-        proc = sp.Popen(["coord_trans"], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True, shell=False).communicate("".join([x + "\n" for x in inlist]))
+        proc = sp.Popen(["coord_trans"], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True,
+                        shell=False).communicate("".join([x + "\n" for x in inlist]))
         proc = [x for x in filter(None, proc[0].split("\n")) if ":" in x]
         self.index = []
         for item in proc:
@@ -86,7 +136,7 @@ class UTM(object):
             self.index.append(entry[0])
             if "UTM" in entry[0]:
                 self.zone, self.northing, self.easting = entry[1]
-                self.index = list(set(self.index+["zone", "northing", "easting"]))
+                self.index = list(set(self.index + ["zone", "northing", "easting"]))
 
 
 def process(cmd, outdir=None, logpath=None, inlist=None, void=True):
@@ -104,20 +154,20 @@ class Spacing(object):
         """
         # compute ground range pixel spacing
         par = par if isinstance(par, ISPPar) else ISPPar(par)
-        self.groundRangePS = par.range_pixel_spacing/(math.sin(math.radians(par.incidence_angle)))
+        self.groundRangePS = par.range_pixel_spacing / (math.sin(math.radians(par.incidence_angle)))
         # compute initial multilooking factors
         if targetres == "automatic":
             if self.groundRangePS > par.azimuth_pixel_spacing:
-                ratio = self.groundRangePS/par.azimuth_pixel_spacing
+                ratio = self.groundRangePS / par.azimuth_pixel_spacing
                 self.rlks = 1
                 self.azlks = int(round(ratio))
             else:
-                ratio = par.azimuth_pixel_spacing/self.groundRangePS
+                ratio = par.azimuth_pixel_spacing / self.groundRangePS
                 self.rlks = int(round(ratio))
                 self.azlks = 1
         else:
-            self.rlks = int(round(float(targetres)/self.groundRangePS))
-            self.azlks = int(round(float(targetres)/par.azimuth_pixel_spacing))
+            self.rlks = int(round(float(targetres) / self.groundRangePS))
+            self.azlks = int(round(float(targetres) / par.azimuth_pixel_spacing))
 
 
 class Namespace(object):
@@ -128,7 +178,7 @@ class Namespace(object):
 
     def appreciate(self, keys):
         for key in keys:
-            setattr(self, key.replace('.', '_'), os.path.join(self.__outdir, self.__base+'_'+key))
+            setattr(self, key.replace('.', '_'), os.path.join(self.__outdir, self.__base + '_' + key))
             if key not in self.__reg:
                 self.__reg.append(key.replace('.', '_'))
 
