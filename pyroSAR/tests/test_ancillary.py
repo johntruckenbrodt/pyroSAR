08c4b674e94fb149b04bee2622057386afefc5d8
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-07-12 16:23:35 +0200

    [test_ancillary.test_which] adjusted to new function parameter mode

--- pyroSAR/tests/test_ancillary.py
+++ pyroSAR/tests/test_ancillary.py
@@ -42,9 +42,9 @@ def test_run(tmpdir, testdata):
 def test_which():
     env = os.environ['PATH']
     os.environ['PATH'] = '{}{}{}'.format(os.environ['PATH'], os.path.pathsep, os.path.dirname(os.__file__))
-    program = anc.which(os.__file__)
+    program = anc.which(os.__file__, os.F_OK)
     assert os.path.isfile(program)
-    assert anc.which(program) == program
+    assert anc.which(program, os.F_OK) == program
     assert anc.which('foobar') is None
     os.environ['PATH'] = env
 
