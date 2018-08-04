9406a618aa86401ad21062eb5967f9fafa70d3e6
Ismail Baris <i.baris@outlook.de>
2018-08-04 12:58:24 +0200

    [tests.test_dev_config] Add unit test for ConfigHandler

--- pyroSAR/tests/test_dev_config.py
+++ pyroSAR/tests/test_dev_config.py
@@ -1,6 +1,6 @@
+from pyroSAR._dev_config import Storage, LOOKUP, URL, STORAGE, ConfigHandler
+import os
 import pytest
-
-from pyroSAR._dev_config import Storage, LOOKUP, URL, STORAGE
 
 
 class TestStorage:
@@ -16,6 +16,7 @@ class TestStorage:
         assert key[0] == 'a' or 'b'
         assert key[1] == 'b' or 'a'
 
+
 class TestLookup:
     def test_suffix(self):
         assert LOOKUP.snap.suffix['Apply-Orbit-File'] == 'Orb'
@@ -24,6 +25,7 @@ class TestLookup:
     def test_attributes(self):
         assert LOOKUP.attributes['sensor'] == 'TEXT'
         assert LOOKUP.attributes['vh'] == 'INTEGER'
+
 
 class TestSTORAGE:
     def test_STORAGE_URL(self):
@@ -36,3 +38,68 @@ class TestSTORAGE:
         assert LOOKUP.snap.suffix['Terrain-Correction'] == STORAGE.LOOKUP.snap.suffix['Terrain-Correction'] == 'TC'
         assert LOOKUP.attributes['sensor'] == STORAGE.LOOKUP.attributes['sensor']
         assert LOOKUP.attributes['vh'] == STORAGE.LOOKUP.attributes['vh']
+
+
+class TestConfigHandler:
+
+    def test_make_dir_and_config(self):
+        conf = ConfigHandler(config_fname='unit_test_config.ini')
+
+        path_pyrosar = os.path.exists(conf._ConfigHandler__GLOBAL['path'])
+        path_config = os.path.isfile(conf._ConfigHandler__GLOBAL['config'])
+
+        assert path_pyrosar == True
+        assert path_config == True
+
+    def test_add_section(self):
+        conf = ConfigHandler(config_fname='unit_test_config.ini')
+        conf.add_section('SNAP')
+
+        assert conf.sections[0] == 'SNAP'
+
+        with pytest.raises(AttributeError) as excinfo:
+            conf.add_section('SNAPP')
+        assert "['SNAP']" in str(excinfo)
+
+    def test_options(self):
+        conf = ConfigHandler(config_fname='unit_test_config.ini')
+        conf.add_section('SNAP')
+        conf.set('SNAP', 'etc', 'temp/dir')
+
+        with pytest.raises(AttributeError) as excinfo:
+            conf.set('SNAPp', 'etc', 'temp/dir')
+
+        assert conf.SNAP['etc'] == 'temp/dir'
+        assert conf.get('SNAP')['etc'] == 'temp/dir'
+        assert conf.get('SNAP') == {'etc': 'temp/dir'}
+        assert conf.keys('SNAP') == ['etc']
+        assert conf.sections == ['SNAP']
+
+    def test_overwrite(self):
+        conf = ConfigHandler(config_fname='unit_test_config.ini')
+        conf.add_section('SNAP')
+        conf.set('SNAP', 'etc', 'temp/dir')
+        assert conf.SNAP['etc'] == 'temp/dir'
+
+        conf.set('SNAP', 'etc', 'temp/dir2', True)
+        assert conf.SNAP['etc'] == 'temp/dir2'
+
+    def test_remove(self):
+        conf = ConfigHandler(config_fname='unit_test_config.ini')
+        conf.add_section('SNAP')
+        conf.set('SNAP', 'etc', 'temp/dir')
+
+        with pytest.raises(AttributeError) as excinfo:
+            conf.remove_option('SNAP', 'kex')
+
+        with pytest.raises(AttributeError) as excinfo:
+            conf.remove_option('SNApP', 'etc')
+
+        conf.remove_option('SNAP', 'etc')
+        conf.keys('SNAP')
+        assert conf.keys('SNAP') == []
+
+    def test_delete_unit_data(self):
+        conf = ConfigHandler(config_fname='unit_test_config.ini')
+
+        os.remove(conf._ConfigHandler__GLOBAL['config'])
