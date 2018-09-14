6a4ca1b821fbbfbc83e940236197a27bdac74ed0
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-09-14 19:00:53 +0200

    [datacube] new classes Product and Dataset

--- pyroSAR/datacube.py
+++ pyroSAR/datacube.py
@@ -1,6 +1,9 @@
+import os
 import re
 import yaml
+import uuid
 from spatialist.raster import Raster, Dtype
+from spatialist.ancillary import union
 from .ancillary import parse_datasetname
 
 
@@ -132,3 +135,224 @@ def parse_product_yaml(name, type, description, units, datasets, ymlfile):
     # write YAML file
     with open(ymlfile, 'w') as yml:
         yaml.dump(metadata, yml, default_flow_style=False)
+
+
+class Dataset(object):
+    def __init__(self, filename):
+        
+        self.filename = os.path.realpath(filename)
+        
+        sensor_lookup = {'ASAR': ('ENVISAT', 'ASAR'),
+                         'ERS1': ('ERS-1', 'SAR'),
+                         'ERS2': ('ERS-2', 'SAR'),
+                         'PSR1': ('ALOS-1', 'PALSAR'),
+                         'PSR2': ('ALOS-2', 'PALSAR-2'),
+                         'S1A': ('SENTINEL-1', 'C-SAR'),
+                         'S1B': ('SENTINEL-1', 'C-SAR'),
+                         'TSX1': ('TERRASAR-X_1', 'SAR'),
+                         'TDX1': ('TANDEM-X_1', 'SAR')}
+        
+        meta = parse_datasetname(filename)
+        
+        if meta is None:
+            raise ValueError('could not identify dataset: {}'.format(filename))
+        
+        for key, val in meta.items():
+            setattr(self, key, val)
+        
+        self.measurement = self.polarization
+        
+        if self.sensor not in sensor_lookup.keys():
+            raise ValueError('unknown sensor: {}'.format(self.sensor))
+        
+        self.platform, self.instrument = sensor_lookup[self.sensor]
+        
+        with Raster(filename) as ras:
+            self.dtype = Dtype(ras.dtype).numpystr
+            self.nodata = ras.nodata
+            self.format = ras.format
+            self.xres, self.yres = ras.res
+            self.crs = 'EPSG:{}'.format(ras.epsg)
+            self.is_projected = ras.projcs is not None
+            self.extent = {'ll': {'x': ras.geo['xmin'],
+                                  'y': ras.geo['ymin']},
+                           'lr': {'x': ras.geo['xmax'],
+                                  'y': ras.geo['ymin']},
+                           'ul': {'x': ras.geo['xmin'],
+                                  'y': ras.geo['ymax']},
+                           'ur': {'x': ras.geo['xmax'],
+                                  'y': ras.geo['ymax']}}
+        
+        # create dictionary for resolution metadata depending on CRS characteristics
+        resolution_keys = ('x', 'y') if self.is_projected else ('longitude', 'latitude')
+        self.resolution = dict(zip(resolution_keys, (self.xres, self.yres)))
+        
+        # check whether the data type is supported
+        pattern = '(?:(?:u|)int(?:8|16|32|64)|float(?:32|64))'
+        if not re.search(pattern, self.dtype):
+            raise ValueError('unsupported data type {}'.format(self.dtype))
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+    
+    def close(self):
+        return
+
+
+class Product(object):
+    def __init__(self, definition=None, name=None, product_type=None,
+                 description=None, measurement_units=None):
+        self.measurement_units = measurement_units if measurement_units is not None else {}
+        
+        missing_message = "when initializing from {}, parameters " \
+                          "'name', 'product_type' and 'description' must be defined"
+        
+        if isinstance(definition, str) and os.path.isfile(definition):
+            with open(definition, 'r') as yml:
+                try:
+                    self.meta = yaml.load(yml)
+                except yaml.YAMLError:
+                    raise RuntimeError('the provided file does not seem to be a YAML file')
+        
+        elif isinstance(definition, list):
+            if None in [name, product_type, description]:
+                raise ValueError(missing_message.format('list'))
+            self.__initialize(name, product_type, description)
+            for dataset in definition:
+                with Dataset(dataset) as DS:
+                    self.add(DS)
+        
+        elif definition is None:
+            self.__initialize(name, product_type, description)
+        else:
+            raise TypeError('tpye of parameter definition must be either str, list or None')
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+    
+    def __str__(self):
+        return yaml.dump(self.meta, default_flow_style=False)
+    
+    def close(self):
+        return
+    
+    def __add_measurement(self, name, dtype, nodata):
+        if name not in self.measurement_units.keys():
+            self.measurement_units[name] = 'DN'
+        if name in self.measurements.keys():
+            raise IndexError('measurement {} already exists'.format(name))
+        self.meta['measurements'].append({'name': name,
+                                          'dtype': dtype,
+                                          'units': self.measurement_units[name],
+                                          'nodata': nodata})
+    
+    def __initialize(self, name, product_type, description):
+        self.meta = {'description': description,
+                     'measurements': [],
+                     'metadata': {'product_type': product_type},
+                     'metadata_type': 'eo',
+                     'name': name,
+                     'storage': {}}
+    
+    @staticmethod
+    def __check_dict_keys(keys, reference):
+        return len(union(keys, reference)) == len(keys)
+    
+    def __check_validity(self):
+        try:
+            assert isinstance(self.meta, dict)
+            assert self.__check_dict_keys(self.__fixture_fields, self.meta.keys())
+            assert 'product_type' in self.meta['metadata'].keys()
+            for measurement in self.meta['measurements']:
+                assert self.__check_dict_keys(self.__fixture_measurement, measurement.keys())
+        except AssertionError as e:
+            print(e)
+            raise RuntimeError('product invalid')
+    
+    @property
+    def __fixture_fields(self):
+        return ['description', 'measurements', 'metadata', 'metadata_type', 'name', 'storage']
+    
+    @property
+    def __fixture_measurement(self):
+        return ['dtype', 'nodata']
+    
+    @property
+    def __fixture_metadata(self):
+        return ['format', 'instrument', 'platform']
+    
+    @property
+    def __fixture_storage(self):
+        return ['crs', 'resolution']
+    
+    def add(self, dataset):
+        if not isinstance(dataset, Dataset):
+            raise TypeError('input must be of type pyroSAR.datacube.Dataset')
+        self.check_integrity(dataset, allow_new_measurements=True)
+        for attr in self.__fixture_metadata:
+            self.meta['metadata'][attr] = {'name': getattr(dataset, attr)}
+        
+        for attr in self.__fixture_storage:
+            self.meta['storage'][attr] = getattr(dataset, attr)
+        
+        if dataset.measurement not in self.measurements.keys():
+            self.__add_measurement(dataset.measurement, dataset.dtype, dataset.nodata)
+    
+    def check_integrity(self, dataset, allow_new_measurements=False):
+        
+        # define parameters, which are to be consistent across an individual measurement
+        # fixtures_measurement = ['acquisition_mode', 'proc_steps', 'dtype', 'nodata']
+        
+        # check metadata field
+        for attr in self.__fixture_metadata:
+            if attr in self.meta['metadata'].keys():
+                if getattr(dataset, attr) != self.meta['metadata'][attr]['name']:
+                    raise ValueError('mismatch of attribute {}'.format(attr))
+        
+        # check storage field
+        for attr in self.__fixture_storage:
+            if attr in self.meta['storage'].keys():
+                if getattr(dataset, attr) != self.meta['storage'][attr]:
+                    raise ValueError('mismatch of attribute {}'.format(attr))
+        
+        if dataset.measurement not in self.measurements.keys():
+            if not allow_new_measurements:
+                raise ValueError('measurement mismatch')
+        else:
+            match = self.measurements[dataset.measurement]
+            for attr in self.__fixture_measurement:
+                if match[attr] != getattr(dataset, attr):
+                    raise ValueError('mismatch of attribute {}'.format(attr))
+    
+    def export_ingestion_yml(self, dataset):
+        self.check_integrity(dataset)
+        out = {'id': str(uuid.uuid4()),
+               'image': {'bands': {}},
+               'grid_spatial': {'projection': {}},
+               'lineage': {'source_datasets': {}}}
+        
+        out['image']['bands'][dataset.measurement] = {'path': dataset.filename}
+        
+        for attr in self.__fixture_metadata:
+            out[attr] = {'name': getattr(dataset, attr)}
+        
+        out['grid_spatial']['projection']['geo_ref_points'] = dataset.extent
+        out['grid_spatial']['projection']['spatial_reference'] = dataset.crs
+        
+        out['product_type'] = self.meta['metadata']['product_type']
+        
+        print(yaml.dump(out, default_flow_style=False))
+    
+    @property
+    def measurements(self):
+        return dict([(x['name'], x) for x in self.meta['measurements']])
+    
+    def write(self, ymlfile):
+        with open(ymlfile, 'w') as yml:
+            yaml.dump(self.meta, yml, default_flow_style=False)
