18fe407a43081ccba44ec9b3106d596b40c3ed43
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-09-18 15:18:04 +0200

    [datacube_util.Dataset] several changes to support the following:
    - pass a list of files upon initialization
    - pass the measurement units upon initialization
    - allow combination of datasets via + operator and sum function
    - set units of all measurements in the dataset
    - get measurement parameters as dictionary

--- pyroSAR/datacube_util.py
+++ pyroSAR/datacube_util.py
@@ -8,55 +8,97 @@ from .ancillary import parse_datasetname
 
 
 class Dataset(object):
-    def __init__(self, filename):
+    def __init__(self, filename, units='DN'):
         
-        self.filename = os.path.realpath(filename)
+        if isinstance(filename, list):
+            combined = sum([Dataset(x, units) for x in filename])
+            self.__init__(combined)
         
-        sensor_lookup = {'ASAR': ('ENVISAT', 'ASAR'),
-                         'ERS1': ('ERS-1', 'SAR'),
-                         'ERS2': ('ERS-2', 'SAR'),
-                         'PSR1': ('ALOS-1', 'PALSAR'),
-                         'PSR2': ('ALOS-2', 'PALSAR-2'),
-                         'S1A': ('SENTINEL-1', 'C-SAR'),
-                         'S1B': ('SENTINEL-1', 'C-SAR'),
-                         'TSX1': ('TERRASAR-X_1', 'SAR'),
-                         'TDX1': ('TANDEM-X_1', 'SAR')}
+        elif isinstance(filename, Dataset):
+            for attr, value in vars(filename).items():
+                setattr(self, attr, value)
         
-        meta = parse_datasetname(filename)
-        
-        if meta is None:
-            raise ValueError('could not identify dataset: {}'.format(filename))
-        
-        for key, val in meta.items():
-            setattr(self, key, val)
-        
-        self.measurement = self.polarization
-        
-        if self.sensor not in sensor_lookup.keys():
-            raise ValueError('unknown sensor: {}'.format(self.sensor))
-        
-        self.platform, self.instrument = sensor_lookup[self.sensor]
-        
-        with Raster(filename) as ras:
-            self.dtype = Dtype(ras.dtype).numpystr
-            self.nodata = ras.nodata
-            self.format = ras.format
-            self.xres, self.yres = ras.res
-            self.crs = 'EPSG:{}'.format(ras.epsg)
-            self.is_projected = ras.projcs is not None
-            self.extent = self.__extent_convert(ras.geo, 'x', 'y')
-            with ras.bbox() as bbox:
-                bbox.reproject(4236)
-                self.extent_4326 = self.__extent_convert(bbox.extent, 'lon', 'lat')
-        
-        # create dictionary for resolution metadata depending on CRS characteristics
-        resolution_keys = ('x', 'y') if self.is_projected else ('longitude', 'latitude')
-        self.resolution = dict(zip(resolution_keys, (self.xres, self.yres)))
-        
-        # check whether the data type is supported
-        pattern = '(?:(?:u|)int(?:8|16|32|64)|float(?:32|64))'
-        if not re.search(pattern, self.dtype):
-            raise ValueError('unsupported data type {}'.format(self.dtype))
+        elif isinstance(filename, str):
+            sensor_lookup = {'ASAR': ('ENVISAT', 'ASAR'),
+                             'ERS1': ('ERS-1', 'SAR'),
+                             'ERS2': ('ERS-2', 'SAR'),
+                             'PSR1': ('ALOS-1', 'PALSAR'),
+                             'PSR2': ('ALOS-2', 'PALSAR-2'),
+                             'S1A': ('SENTINEL-1', 'C-SAR'),
+                             'S1B': ('SENTINEL-1', 'C-SAR'),
+                             'TSX1': ('TERRASAR-X_1', 'SAR'),
+                             'TDX1': ('TANDEM-X_1', 'SAR')}
+            
+            meta = parse_datasetname(filename)
+            
+            if meta is None:
+                raise ValueError('could not identify dataset: {}'.format(filename))
+            
+            for key, val in meta.items():
+                setattr(self, key, val)
+            
+            if self.sensor not in sensor_lookup.keys():
+                raise ValueError('unknown sensor: {}'.format(self.sensor))
+            
+            self.platform, self.instrument = sensor_lookup[self.sensor]
+            
+            with Raster(filename) as ras:
+                self.dtype = Dtype(ras.dtype).numpystr
+                self.nodata = ras.nodata
+                self.format = ras.format
+                self.xres, self.yres = ras.res
+                self.crs = 'EPSG:{}'.format(ras.epsg)
+                self.is_projected = ras.projcs is not None
+                self.extent = self.__extent_convert(ras.geo, 'x', 'y')
+                with ras.bbox() as bbox:
+                    bbox.reproject(4326)
+                    self.extent_4326 = self.__extent_convert(bbox.extent, 'lon', 'lat')
+            
+            # create dictionary for resolution metadata depending on CRS characteristics
+            resolution_keys = ('x', 'y') if self.is_projected else ('longitude', 'latitude')
+            self.resolution = dict(zip(resolution_keys, (self.xres, self.yres)))
+            
+            # check whether the data type is supported
+            pattern = '(?:(?:u|)int(?:8|16|32|64)|float(?:32|64))'
+            if not re.search(pattern, self.dtype):
+                raise ValueError('unsupported data type {}'.format(self.dtype))
+            
+            # determine the dataset units
+            if isinstance(units, str):
+                units = units
+            elif isinstance(units, dict):
+                try:
+                    units = units[self.polarization]
+                except KeyError:
+                    raise KeyError("parameter 'units' does not contain key '{}'".format(self.polarization))
+            else:
+                raise TypeError("parameter 'units' must be of type str or dict")
+            
+            # create the measurement entry from collected metadata; this is intended for easy access by class Product
+            self.measurements = {self.polarization: {'dtype': self.dtype,
+                                                     'name': self.polarization,
+                                                     'nodata': self.nodata,
+                                                     'filename': filename,
+                                                     'units': units}}
+        else:
+            raise TypeError('filename must be of type str, list or Dataset')
+    
+    def __add__(self, dataset):
+        for attr in ['extent', 'crs', 'sensor', 'acquisition_mode', 'proc_steps', 'outname_base']:
+            if getattr(self, attr) != getattr(dataset, attr):
+                raise ValueError('value mismatch: {}'.format(attr))
+        # self.filename.append(dataset.filename)
+        for key in dataset.measurements.keys():
+            if key in self.measurements.keys():
+                raise RuntimeError('only different measurements can be combined to one dataset')
+        self.measurements.update(dataset.measurements)
+        return self
+    
+    def __radd__(self, dataset):
+        if dataset == 0:
+            return self
+        else:
+            return self.__add__(dataset)
     
     @staticmethod
     def __extent_convert(extent, xkey, ykey):
@@ -74,6 +116,37 @@ class Dataset(object):
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
+    
+    def __get_measurement_attr(self, attr):
+        return dict([(key, self.measurements[key][attr]) for key in self.measurements.keys()])
+    
+    @property
+    def filenames(self):
+        return self.__get_measurement_attr('filename')
+    
+    @property
+    def identifier(self):
+        return '{}_{}'.format(self.outname_base, '_'.join(self.proc_steps))
+    
+    @property
+    def units(self):
+        return self.__get_measurement_attr('units')
+    
+    @units.setter
+    def units(self, value):
+        keys = list(self.measurements.keys())
+        if isinstance(value, str):
+            if len(keys) == 1:
+                self.measurements[keys[0]]['units'] = value
+            else:
+                raise TypeError('the dataset contains multiple measurements; '
+                                'in this case a dictionary is needed for setting the measurement units')
+        elif isinstance(value, dict):
+            for name, unit in value.items():
+                if name in keys:
+                    self.measurements[name]['units'] = unit
+                else:
+                    raise KeyError("the dataset does not contain a measurement '{}'".format(name))
     
     def close(self):
         return
