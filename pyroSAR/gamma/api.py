5b67eb62985477160c6017ad4118f6f3bed2aa0c
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-10-08 09:52:03 +0200

    [gamma.api] issue warning messages

--- pyroSAR/gamma/api.py
+++ pyroSAR/gamma/api.py
@@ -6,13 +6,12 @@ from .parser import autoparse
 
 try:
     autoparse()
-
 except RuntimeError:
-    warnings.warn('could not find Gamma installation; module autoparsing failed')
+    warnings.warn('could not find Gamma installation directory; please set the GAMMA_HOME environment variable')
 
 sys.path.insert(0, os.path.join(os.path.expanduser('~'), '.pyrosar'))
 
 try:
     from gammaparse import *
 except ImportError:
-    pass
+    warnings.warn('found a Gamma installation directory, but module parsing failed')
