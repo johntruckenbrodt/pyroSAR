f1469eea8a4cc1e0c81a73ebc0fe983e5fabf510
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-02-12 10:07:05 +0100

    [test_ancillary] added tests for parse_datasetname and find_datasets

--- pyroSAR/tests/test_ancillary.py
+++ pyroSAR/tests/test_ancillary.py
@@ -1,4 +1,5 @@
-from pyroSAR.ancillary import seconds, groupbyTime, groupby
+import datetime
+from pyroSAR.ancillary import seconds, groupbyTime, groupby, parse_datasetname, find_datasets
 
 
 def test_seconds():
@@ -19,9 +20,9 @@ def test_groupby():
     assert len(sensor_groups) == 2
     assert isinstance(sensor_groups[0], list)
     assert len(sensor_groups[0]) == 2
-
+    
     filenames += ['S2A__IW___A_20180309T173017_VV_grd_mli_geo_norm_db.tif']
-
+    
     polarization_groups = groupby(filenames, 'polarization')
     print(polarization_groups)
     assert len(polarization_groups) == 2
@@ -29,9 +30,9 @@ def test_groupby():
     assert isinstance(polarization_groups[1], list)
     assert len(polarization_groups[0]) == 2
     assert len(polarization_groups[1]) == 2
-
+    
     filenames += ['S2A__IW___A_20180309T173017_HV_grd_mli_geo_norm_db.tif']
-
+    
     polarization_groups = groupby(filenames, 'polarization')
     print(polarization_groups)
     assert len(polarization_groups) == 3
@@ -52,10 +53,35 @@ def test_groupbyTime():
     assert len(groups) == 2
     assert isinstance(groups[0], list)
     assert len(groups[0]) == 2
-
+    
     filenames = ['S1__IW___A_20151212T120000',
                  'S1__IW___A_20151212T120100',
                  'S1__IW___A_20151212T120200']
     groups = groupbyTime(filenames, seconds, 60)
     print(groups)
     assert len(groups[0]) == 3
+
+
+def test_parse_datasetname():
+    assert parse_datasetname('foobar') is None
+    filename = 'S1A__IW___A_20150309T173017_VV_grd_mli_geo_norm_db.tif'
+    meta = parse_datasetname(filename, parse_date=True)
+    assert sorted(meta.keys()) == ['acquisition_mode', 'extensions', 'filename',
+                                   'orbit', 'outname_base', 'polarization',
+                                   'proc_steps', 'sensor', 'start']
+    assert meta['acquisition_mode'] == 'IW'
+    assert meta['extensions'] is None
+    assert meta['filename'] == filename
+    assert meta['orbit'] == 'A'
+    assert meta['outname_base'] == 'S1A__IW___A_20150309T173017'
+    assert meta['polarization'] == 'VV'
+    assert meta['proc_steps'] == ['grd', 'mli', 'geo', 'norm', 'db']
+    assert meta['sensor'] == 'S1A'
+    assert meta['start'] == datetime.datetime(2015, 3, 9, 17, 30, 17)
+    meta = parse_datasetname('S1A__IW___A_20150309T173017_VV_grd.tif')
+    assert meta['proc_steps'] == ['grd']
+
+
+def test_find_datasets(testdir):
+    assert len(find_datasets(testdir, sensor='S1A')) == 1
+    assert len(find_datasets(testdir, sensor='S1B')) == 0
