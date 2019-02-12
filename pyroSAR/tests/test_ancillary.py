718bf47ed50d750d02a111bbc0a4d516b182351f
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-02-12 09:19:03 +0100

    [test_ancillary] removed tests for functionality moved to spatialist

--- pyroSAR/tests/test_ancillary.py
+++ pyroSAR/tests/test_ancillary.py
@@ -1,86 +1,8 @@
-import os
-import pytest
-import subprocess as sp
-import spatialist.ancillary as anc
 from pyroSAR.ancillary import seconds, groupbyTime, groupby
-
-
-def test_dissolve_with_lists():
-    assert anc.dissolve([[1, 2], [3, 4]]) == [1, 2, 3, 4]
-    assert anc.dissolve([[[1]]]) == [1]
-    assert anc.dissolve(((1, 2,), (3, 4))) == [1, 2, 3, 4]
-    assert anc.dissolve(((1, 2), (1, 2))) == [1, 2, 1, 2]
-
-
-def test_union():
-    assert anc.union([1], [1]) == [1]
-
-
-def test_parse_literal():
-    assert anc.parse_literal(['1', '2.2', 'a']) == [1, 2.2, 'a']
-    with pytest.raises(TypeError):
-        anc.parse_literal(1)
 
 
 def test_seconds():
     assert seconds('test_20151212T234411') == 3658952651.0
-
-
-def test_run(tmpdir, testdata):
-    log = os.path.join(str(tmpdir), 'test_run.log')
-    out, err = anc.run(cmd=['gdalinfo', testdata['tif']],
-                       logfile=log, void=False)
-    with pytest.raises(OSError):
-        anc.run(['foobar'])
-    with pytest.raises(sp.CalledProcessError):
-        anc.run(['gdalinfo', 'foobar'])
-
-
-def test_which():
-    env = os.environ['PATH']
-    os.environ['PATH'] = '{}{}{}'.format(os.environ['PATH'], os.path.pathsep, os.path.dirname(os.__file__))
-    program = anc.which(os.__file__, os.F_OK)
-    assert os.path.isfile(program)
-    assert anc.which(program, os.F_OK) == program
-    assert anc.which('foobar') is None
-    os.environ['PATH'] = env
-
-
-def test_multicore():
-    add = lambda x, y, z: x + y + z
-    assert anc.multicore(add, cores=2, multiargs={'x': [1, 2]}, y=5, z=9) == [15, 16]
-    assert anc.multicore(add, cores=2, multiargs={'x': [1, 2], 'y': [5, 6]}, z=9) == [15, 17]
-    with pytest.raises(AttributeError):
-        anc.multicore(add, cores=2, multiargs={'foobar': [1, 2]}, y=5, z=9)
-    with pytest.raises(AttributeError):
-        anc.multicore(add, cores=2, multiargs={'x': [1, 2]}, y=5, foobar=9)
-    with pytest.raises(AttributeError):
-        anc.multicore(add, cores=2, multiargs={'x': [1, 2], 'y': [5, 6, 7]}, foobar=9)
-
-
-def test_finder(tmpdir):
-    dir = str(tmpdir)
-    dir_sub1 = os.path.join(dir, 'testdir1')
-    dir_sub2 = os.path.join(dir, 'testdir2')
-    os.makedirs(dir_sub1)
-    os.makedirs(dir_sub2)
-    with open(os.path.join(dir_sub1, 'testfile1.txt'), 'w') as t1:
-        t1.write('test')
-    with open(os.path.join(dir_sub2, 'testfile2.txt'), 'w') as t2:
-        t2.write('test')
-    assert len(anc.finder(dir, ['test*'], foldermode=0)) == 2
-    assert len(anc.finder(dir, ['test*'], foldermode=0, recursive=False)) == 0
-    assert len(anc.finder(dir, ['test*'], foldermode=1)) == 4
-    assert len(anc.finder(dir, ['test*'], foldermode=2)) == 2
-    assert len(anc.finder([dir_sub1, dir_sub2], ['test*'])) == 2
-    with pytest.raises(TypeError):
-        anc.finder(1, [])
-
-
-def test_rescale():
-    assert anc.rescale([1000, 2000, 3000], [1, 3]) == [1, 2, 3]
-    with pytest.raises(RuntimeError):
-        anc.rescale([1000, 1000])
 
 
 def test_groupby():
