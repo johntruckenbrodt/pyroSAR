73c6b56f24ffc7bb1401672e58766ceec5bd781e
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-03-14 18:30:01 +0100

    [test_snap] adjusted to new workflow handler classes

--- pyroSAR/tests/test_snap.py
+++ pyroSAR/tests/test_snap.py
@@ -4,8 +4,7 @@ from pyroSAR import identify
 from pyroSAR.snap import geocode
 from spatialist import bbox
 from spatialist.ancillary import finder
-import xml.etree.ElementTree as ET
-from pyroSAR.snap.auxil import is_consistent, split, groupbyWorkers, parse_suffix, ExamineSnap
+from pyroSAR.snap.auxil import is_consistent, split, groupbyWorkers, ExamineSnap, Workflow
 
 
 def test_installation():
@@ -17,14 +16,13 @@ def test_geocode(tmpdir, testdata):
     scene = testdata['s1']
     geocode(scene, str(tmpdir), test=True)
     xmlfile = finder(str(tmpdir), ['*.xml'])[0]
-    with open(xmlfile, 'r') as infile:
-        tree = ET.fromstring(infile.read())
-    nodes = tree.findall('node')
+    tree = Workflow(xmlfile)
+    nodes = tree.nodes()
     assert is_consistent(nodes) is True
     groups = groupbyWorkers(xmlfile, 2)
     split(xmlfile, groups)
     id = identify(scene)
-    basename = '{}_{}'.format(id.outname_base(), parse_suffix(tree))
+    basename = '{}_{}'.format(id.outname_base(), tree.suffix)
     procdir = os.path.join(str(tmpdir), basename)
     assert os.path.isdir(procdir)
     tempdir = os.path.join(procdir, 'temp')
@@ -79,7 +77,7 @@ class Test_geocode_opts():
     
     def test_export_extra(self, tmpdir, testdata):
         scene = testdata['s1']
-        with pytest.raises(AttributeError):
+        with pytest.raises(RuntimeError):
             geocode(scene, str(tmpdir), test=True,
                     export_extra=['foobar'])
         geocode(scene, str(tmpdir), test=True,
