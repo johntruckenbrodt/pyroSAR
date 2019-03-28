d735446fe920088f4c03f5f190b96832ff1ee2f8
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-03-28 14:08:56 +0100

    [test_auxdata] adjusted tests to check for 1-deg AW3D30 products

--- pyroSAR/tests/test_auxdata.py
+++ pyroSAR/tests/test_auxdata.py
@@ -7,7 +7,7 @@ from spatialist import bbox
 def test_handler():
     with bbox({'xmin': 11.5, 'xmax': 11.9, 'ymin': 51.1, 'ymax': 51.5}, crs=4326) as box:
         with DEMHandler([box]) as handler:
-            cases = [('AW3D30', ['N050E010_N055E015.tar.gz']),
+            cases = [('AW3D30', ['N050E010/N051E011.tar.gz']),
                      ('SRTM 1Sec HGT', ['N51E011.SRTMGL1.hgt.zip']),
                      ('SRTM 3Sec', ['srtm_39_02.zip']),
                      ('TDX90m', ['90mdem/DEM/N51/E010/TDM1_DEM__30_N51E011.zip'])]
@@ -17,7 +17,7 @@ def test_handler():
     
     with bbox({'xmin': -11.9, 'xmax': -11.5, 'ymin': -51.5, 'ymax': -51.1}, crs=4326) as box:
         with DEMHandler([box]) as handler:
-            cases = [('AW3D30', ['S055W015_S050W010.tar.gz']),
+            cases = [('AW3D30', ['S055W015/S052W012.tar.gz']),
                      ('SRTM 1Sec HGT', ['S52W012.SRTMGL1.hgt.zip']),
                      ('SRTM 3Sec', ['srtm_34_23.zip']),
                      ('TDX90m', ['90mdem/DEM/S52/W010/TDM1_DEM__30_S52W012.zip'])]
