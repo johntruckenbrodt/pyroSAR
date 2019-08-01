b9dbcc3767fdbcf98459f788fdc0623e81297ecc
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-08-01 13:45:25 +0200

    [example_GAMMA_DEM_preparation] updated by using function dem_autocreate

--- examples/example_GAMMA_DEM_preparation.py
+++ examples/example_GAMMA_DEM_preparation.py
@@ -3,14 +3,8 @@
 # John Truckenbrodt 2017-2019
 ##############################################################
 
-import os
-import shutil
-
-from pyroSAR import gamma
-from pyroSAR.gamma import dem, par2hdr
-from spatialist import vector
-
-from spatialist.ancillary import run
+from pyroSAR.gamma.dem import dem_autocreate
+from spatialist import Vector
 
 
 def main():
@@ -24,47 +18,22 @@ def main():
     buffer = 0.01
     
     # load the defined shapefile
-    with vector.Vector(shp) as site:
+    with Vector(shp) as site:
+        
         # reproject the shapefile to latlon (in-memory, no file written or modified)
-        site.reproject('+proj=longlat +datum=WGS84 +no_defs ')
-
-        # extract the extent (bounding box) of the shapefile
-        ext = site.extent
-    
-    # add the buffer to the bounding box
-    ext['xmin'] -= buffer
-    ext['ymin'] -= buffer
-    ext['xmax'] += buffer
-    ext['ymax'] += buffer
-    
-    # define a GDAL VRT file containing all SRTM tiles
-    # this file has all hgt tiles in the same directory registered and is used for subsetting/mosaicing
-    srtm_vrt = '/path/to/SRTM_1_HGT.vrt'
-    
-    # create a temporary directory for writing intermediate files (will be deleted at the end)
-    tmpdir = outname + '__tmp'
-    if not os.path.isdir(tmpdir):
-        os.makedirs(tmpdir)
-    
-    # define a name for a temporary DEM file
-    dem_tmp = os.path.join(tmpdir, 'srtm_tmp.tif')
-    
-    # create a DEM mosaic for the study site
-    run(['gdalwarp', '-q', '-of', 'GTiff', '-te', ext['xmin'], ext['ymin'], ext['xmax'], ext['ymax'], srtm_vrt,
-         dem_tmp])
-    
-    # transform the DEM to GAMMA format (including EGM96 geoid to WGS84 ellipsoid height reference correction)
-    gamma.process(['srtm2dem', dem_tmp, outname, outname + '.par', 2, '-'], outdir=tmpdir)
-    
-    # create an ENVI header file
-    par2hdr(outname + '.par', outname + '.hdr')
-    
-    # remove the temporary directory with all intermediate files
-    shutil.rmtree(tmpdir)
-    
-    # optional: transform DEM to UTM projection
-    # the UTM zone is automatically computed for the center of the DEM file
-    dem.transform(outname, outname + '_utm', posting=20)
+        site.reproject(4326)
+        
+        # create the DEM mosaic in Gamma format including tile download, warping to arbitrary CRS and resolution,
+        # and conversion from geoid to ellipsoid heights
+        # documentation: https://pyrosar.readthedocs.io/en/latest/pyroSAR.html#pyroSAR.gamma.dem.dem_autocreate
+        dem_autocreate(geometry=site,
+                       outfile=outname,
+                       buffer=buffer,
+                       demType='SRTM 1Sec HGT',
+                       t_srs=32632,  # WGS84, UTM 32N
+                       tr=(20, 20),  # 20 m target resolution in both x and y
+                       geoid_mode='gamma',  # convert from EGM96 geoid to WGS84 ellipsoid heights using Gamma
+                       resampling_method='bilinear')
 
 
 if __name__ == '__main__':
