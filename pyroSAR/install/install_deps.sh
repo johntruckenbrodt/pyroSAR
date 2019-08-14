ff671fd2eabdf052fe786ba35f5793632229842b
johntruckenbrodt <john.truckenbrodt@uni-jena.de>
2019-08-14 19:55:27 +0200

    [install_deps.sh] moved shebang to first line

--- pyroSAR/install/install_deps.sh
+++ pyroSAR/install/install_deps.sh
@@ -1,9 +1,9 @@
+#!/usr/bin bash
 ##############################################################
 # manual installation of pyroSAR dependencies
 # GDAL, GEOS, PROJ, SpatiaLite
 # John Truckenbrodt, Rhys Kidd 2017-2019
 ##############################################################
-#!/usr/bin/env bash
 
 
 # define a root directory for downloading packages
