fd4152ed292709649b315c968b3028c5dc27b957
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-12-06 15:09:46 +0100

    [tests.conftest] new s1 test scene

--- pyroSAR/tests/conftest.py
+++ pyroSAR/tests/conftest.py
@@ -21,6 +21,7 @@ def testdir():
 def testdata(testdir):
     out = {
         's1': os.path.join(testdir, 'S1A_IW_GRDH_1SDV_20150222T170750_20150222T170815_004739_005DD8_3768.zip'),
+        's1_2': os.path.join(testdir, 'S1A_IW_GRDH_1SDV_20150222T170725_20150222T170750_004739_005DD8_CEAB.zip'),
         # ftp://ftp.eorc.jaxa.jp/pub/ALOS-2/1501sample/310_forestbrazil/0000022708_001001_ALOS2015976960-140909.zip
         'psr2': os.path.join(testdir, '0000022708_001001_ALOS2015976960-140909.zip'),
         'tif': os.path.join(testdir, 'S1A__IW___A_20150309T173017_VV_grd_mli_geo_norm_db.tif'),
