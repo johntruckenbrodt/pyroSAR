183d4e33e45da07b00fb6b2e70d0a9e10ff57111
corsima <corsima@gmail.com>
2020-10-14 09:41:07 +0200

    Update snap/util.py to fix export_extra option
    The export_extra option seems not to work, commenting the lines fix the problem. I do not know if there is a consequence. The problem seems related to the fact that there is an insert node which add another Terrain-Correction node.

--- pyroSAR/snap/util.py
+++ pyroSAR/snap/util.py
@@ -488,10 +488,10 @@ def geocode(infile, outdir, t_srs=4326, tr=20, polarizations='all', shapefile=No
                    'localIncidenceAngle',
                    'projectedLocalIncidenceAngle',
                    'DEM']
-        write = parse_node('Write')
-        workflow.insert_node(write, before=tc.id, resetSuccessorSource=False)
-        write.parameters['file'] = outname
-        write.parameters['formatName'] = format
+        #write = parse_node('Write')
+        #workflow.insert_node(write, before=tc.id, resetSuccessorSource=False)
+        #write.parameters['file'] = outname
+        #write.parameters['formatName'] = format
         for item in export_extra:
             if item not in options:
                 raise RuntimeError("ID '{}' not valid for argument 'export_extra'".format(item))
