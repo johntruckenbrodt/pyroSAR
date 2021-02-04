82bfaee20419025f2755e20a6534a057975c32b3
johntruckenbrodt <john.truckenbrodt@uni-jena.de>
2021-02-04 11:56:23 +0100

    [snap.geocode] removed function argument 'copyMetadata'

--- pyroSAR/snap/util.py
+++ pyroSAR/snap/util.py
@@ -19,7 +19,7 @@ from .auxil import parse_recipe, parse_node, gpt, groupbyWorkers, get_egm96_look
 from spatialist import crsConvert, Vector, Raster, bbox, intersect
 
 
-def geocode(infile, outdir, t_srs=4326, tr=20, polarizations='all', shapefile=None, copyMetadata=True, scaling='dB',
+def geocode(infile, outdir, t_srs=4326, tr=20, polarizations='all', shapefile=None, scaling='dB',
             geocoding_type='Range-Doppler', removeS1BorderNoise=True, removeS1BorderNoiseMethod='pyroSAR',
             removeS1ThermalNoise=True, offset=None, allow_RES_OSV=False, demName='SRTM 1Sec HGT',
             externalDEMFile=None, externalDEMNoDataValue=None, externalDEMApplyEGM=True, terrainFlattening=True,
@@ -50,8 +50,6 @@ def geocode(infile, outdir, t_srs=4326, tr=20, polarizations='all', shapefile=No
         processed.
     shapefile: str or :py:class:`~spatialist.vector.Vector` or dict or None
         A vector geometry for subsetting the SAR scene to a test site.
-    copyMetadata: bool
-        Set if metadata is retained after the subset operation, necessary if a calibration node follows.
     scaling: {'dB', 'db', 'linear'}, optional
         Should the output be in linear or decibel scaling? Default is 'dB'.
     geocoding_type: {'Range-Doppler', 'SAR simulation cross correlation'}, optional
@@ -469,10 +467,7 @@ def geocode(infile, outdir, t_srs=4326, tr=20, polarizations='all', shapefile=No
         workflow.insert_node(subset, before=read.id)
         subset.parameters['region'] = [0, 0, id.samples, id.lines]
         subset.parameters['geoRegion'] = wkt
-        if copyMetadata:
-            subset.parameters['copyMetadata'] = 'True'
-        else:
-            subset.parameters['copyMetadata'] = 'False'
+        subset.parameters['copyMetadata'] = True
     ############################################
     # (optionally) configure subset node for pixel offsets
     if offset and not shapefile:
