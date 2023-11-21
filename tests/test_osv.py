314b6d625ad730c85be4f6cec10d4dd2860802bc
John Truckenbrodt <john.truckenbrodt@posteo.de>
2023-11-21 17:58:33 +0100

    [test_scene_osv] change url option

--- tests/test_osv.py
+++ tests/test_osv.py
@@ -36,8 +36,7 @@ def test_scene_osv(tmpdir, testdata):
         for item in osv.getLocals('POE')[1:3]:
             os.remove(item)
         assert len(osv.getLocals('POE')) == 1
-        # res = osv.catch(sensor='S1A', osvtype='RES', start='20180101T120000', stop='20180102T120000')
-        res = osv.catch(sensor='S1A', osvtype='RES', start='20210201T00000', stop='20210201T150000', url_option=2)
+        res = osv.catch(sensor='S1A', osvtype='RES', start='20210201T00000', stop='20210201T150000', url_option=1)
         assert len(res) == 9
         osv.retrieve(res[0:3])
         assert len(osv.getLocals('RES')) == 3
