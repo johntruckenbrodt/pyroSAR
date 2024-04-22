3dbcbbcd7ada92fb9adf7f36e0f2178c57bd5286
John Truckenbrodt <john.truckenbrodt@posteo.de>
2024-04-22 14:52:25 +0200

    [config] removed unnecessary Storage class and variables

--- pyroSAR/config.py
+++ pyroSAR/config.py
@@ -2,7 +2,7 @@
 ###############################################################################
 # pyroSAR configuration handling
 
-# Copyright (c) 2018-2023, the pyroSAR Developers.
+# Copyright (c) 2018-2024, the pyroSAR Developers.
 
 # This file is part of the pyroSAR Project. It is subject to the
 # license terms in the LICENSE.txt file found in the top-level
@@ -19,112 +19,6 @@ import configparser as ConfigParser
 
 __LOCAL__ = ['sensor', 'projection', 'orbit', 'polarizations', 'acquisition_mode',
              'start', 'stop', 'product', 'spacing', 'samples', 'lines']
-
-
-class Storage(dict):
-    """
-    Dict class with point access to store the lookups, pattern and URLs
-
-    Attributes
-    ----------
-    STORAGE.LOOKUP : Storage
-        All lookup table merged in a Storage class instance:
-            * snap : SNAP process.
-            * attributes : Attributes for different sensors.
-    STORAGE.URL : dict (with point access)
-        All URLs for DEMs, orbit files etc.:
-            * dem : URL to download specific DEMs:
-                * strm3
-                * ace2
-                * strm3_FTP
-                * strm1HGT
-                * ace
-            * orbit : URL to download the orbit files:
-                * ers1
-                * ers2
-                * s1_poe
-                * s1_pres
-                * doris
-            * auxcal : URL to download the auxcal data:
-                * s1
-                * envisat
-                * ers
-
-    Note
-    ----
-    There may be additional attributes not listed above depending of the
-    specific solver. Since this class is essentially a subclass of dict
-    with attribute accessors, one can see which attributes are available
-    using the `keys()` method.
-    """
-    
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-    
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
-    
-    def __repr__(self):
-        if self.keys():
-            m = max(map(len, list(self.keys()))) + 1
-            return '\n'.join([k.rjust(m) + ': ' + repr(v)
-                              for k, v in sorted(self.items())])
-        else:
-            return self.__class__.__name__ + '()'
-    
-    def __dir__(self):
-        return list(self.keys())
-
-
-# ==============================================================================
-# LOOKUP
-# ==============================================================================
-LOOKUP = Storage(attributes={'sensor': 'TEXT',
-                             'orbit': 'TEXT',
-                             'acquisition_mode': 'TEXT',
-                             'start': 'TEXT',
-                             'stop': 'TEXT',
-                             'product': 'TEXT',
-                             'samples': 'INTEGER',
-                             'lines': 'INTEGER',
-                             'outname_base': 'TEXT PRIMARY KEY',
-                             'scene': 'TEXT',
-                             'hh': 'INTEGER',
-                             'vv': 'INTEGER',
-                             'hv': 'INTEGER',
-                             'vh': 'INTEGER'})
-
-# ==============================================================================
-# URL
-# ==============================================================================
-dem = Storage(ace2='https://step.esa.int/auxdata/dem/ACE2/5M/',
-              ace='https://step.esa.int/auxdata/dem/ACE30/',
-              srtm3_FTP='xftp.jrc.it',
-              srtm3='https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF',
-              srtm1Hgt='https://step.esa.int/auxdata/dem/SRTMGL1/', )
-
-orbit = Storage(doris='https://step.esa.int/auxdata/orbits/Doris/vor',
-                ers1='https://step.esa.int/auxdata/orbits/ers_precise_orb/ERS1',
-                ers2='https://step.esa.int/auxdata/orbits/ers_precise_orb/ERS2',
-                s1_poe='https://step.esa.int/auxdata/orbits/Sentinel-1/POEORB/',
-                s1_res='https://step.esa.int/auxdata/orbits/Sentinel-1/RESORB/')
-
-auxcal = Storage(s1='https://step.esa.int/auxdata/auxcal/S1/',
-                 envisat='https://step.esa.int/auxdata/auxcal/ENVISAT/',
-                 ers='https://step.esa.int/auxdata/auxcal/ERS/')
-
-URL = Storage(dem=dem,
-              orbit=orbit,
-              auxcal=auxcal)
-
-# ==============================================================================
-# Merge
-# ==============================================================================
-STORAGE = Storage(URL=URL,
-                  LOOKUP=LOOKUP)
 
 
 class Singleton(type):
