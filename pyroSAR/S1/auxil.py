668611c11f95bfa23ae9caef22c5160a34430ac1
John Truckenbrodt <john.truckenbrodt@posteo.de>
2025-04-09 15:18:29 +0200

    added S1C/D in some more cases

--- pyroSAR/S1/auxil.py
+++ pyroSAR/S1/auxil.py
@@ -108,8 +108,8 @@ class OSV(object):
             osvdir = os.path.join(auxdatapath, 'Orbits', 'Sentinel-1')
         self.outdir_poe = os.path.join(osvdir, 'POEORB')
         self.outdir_res = os.path.join(osvdir, 'RESORB')
-        self.pattern = r'S1[AB]_OPER_AUX_(?:POE|RES)ORB_OPOD_[0-9TV_]{48}\.EOF'
-        self.pattern_fine = r'(?P<sensor>S1[AB])_OPER_AUX_' \
+        self.pattern = r'S1[ABCD]_OPER_AUX_(?:POE|RES)ORB_OPOD_[0-9TV_]{48}\.EOF'
+        self.pattern_fine = r'(?P<sensor>S1[ABCD])_OPER_AUX_' \
                             r'(?P<type>(?:POE|RES)ORB)_OPOD_' \
                             r'(?P<publish>[0-9]{8}T[0-9]{6})_V' \
                             r'(?P<start>[0-9]{8}T[0-9]{6})_' \
@@ -392,6 +392,8 @@ class OSV(object):
             
              - 'S1A'
              - 'S1B'
+             - 'S1C'
+             - 'S1D'
              - ['S1A', 'S1B', 'S1C', 'S1D']
         osvtype: str
             the type of orbit files required
