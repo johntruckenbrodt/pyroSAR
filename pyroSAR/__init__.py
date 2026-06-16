f2e351dd00a77ff56c2c2216547aca849f1a4f04
John Truckenbrodt <john.truckenbrodt@posteo.de>
2026-06-16 16:20:58 +0200

    [init] added some missing imports

--- pyroSAR/__init__.py
+++ pyroSAR/__init__.py
@@ -1,6 +1,6 @@
 from .drivers import *
 from .archive import Archive, drop_archive
-from . import ancillary, drivers
+from . import ancillary, auxdata, drivers, examine
 
 from importlib.metadata import version, PackageNotFoundError
 
